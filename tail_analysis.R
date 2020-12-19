71cf5d646c860ffd8412846c90dcbbe4f5d94915
sghosh89 <ghoshshyamolina89@gmail.com>
2020-12-19 20:46:55 +0100

    pipeline

---
+++ tail_analysis.R
@@ -0,0 +1,99 @@
+# This script will do the copula analysis, will generate necessary plots, will show how a species 
+# tail dep. fluctuates with the rest of species in the phyto-plankton community
+
+source("./NonParamStat.R")
+source("./copula_covary.R")
+source("./NonParamStat_matrixplot.R")
+
+# Input:
+# mat: a matrix or dataframe where each target species time series along each column, 
+#         last column for all rest of the aggregated species, rows have name for sampling years
+#resloc: path to save results
+#nbin: 2 (default) to measure tail-dep.
+
+# Output:
+# a list and several plots to be saved in resloc path
+
+tail_analysis<-function(mat, resloc, nbin=2){
+  
+  splist<-vector(mode="list",length=ncol(mat))
+  names(splist)<-colnames(mat)
+  for(i in 1:length(splist)){
+    splist[[i]]<-data.frame(Year=rownames(mat),Dat=mat[,i])
+  }
+  
+  z<-multcall(d_allsp = splist, resloc = resloc, nbin=nbin)
+  zcov<-copula_covary(df=mat,nbin=2)
+  
+  if(tail(colnames(mat),1)=="raresp"){
+    tot_target_sp<-ncol(mat)-1 # last column as aggregated rest of the species
+  }else{
+    tot_target_sp<-ncol(mat)
+  }
+  
+  #----- now combine the results ------------
+  
+  # for spearman
+  zs<-z$spear
+  tsp<-ncol(zs)-ncol(zcov)
+  tempo<-matrix(0.999,nrow=nrow(zcov),ncol=tsp) # 0.999 value will be filled in with black color in the plot
+  zcov<-cbind(zcov,tempo)
+  zs<-rbind(zs,zcov[1,])
+  zs<-cbind(zs,matrix(0.999,nrow=nrow(zs),ncol=tsp)) # 0.999 value will be filled in with black color in the plot
+  rownames(zs)[nrow(zs)]<-"covsp"
+  
+  # for kend
+  zk<-z$kend
+  zk<-rbind(zk,zcov[2,])
+  zk<-cbind(zk,matrix(0.999,nrow=nrow(zk),ncol=tsp)) # 0.999 value will be filled in with black color in the plot
+  rownames(zk)[nrow(zk)]<-"covsp"
+  
+  # for Corl
+  zcl<-z$Corl
+  zcl<-rbind(zcl,zcov[3,])
+  zcl<-cbind(zcl,matrix(0.999,nrow=nrow(zcl),ncol=tsp)) # 0.999 value will be filled in with black color in the plot
+  rownames(zcl)[nrow(zcl)]<-"covsp"
+  
+  # for Coru
+  zcu<-z$Coru
+  zcu<-rbind(zcu,zcov[4,])
+  zcu<-cbind(zcu,matrix(0.999,nrow=nrow(zcu),ncol=tsp)) # 0.999 value will be filled in with black color in the plot
+  rownames(zcu)[nrow(zcu)]<-"covsp"
+  
+  # for posnI
+  zpI<-z$posnI
+  zpI<-rbind(zpI,zcov[5,])
+  zpI<-cbind(zpI,matrix(0.999,nrow=nrow(zpI),ncol=tsp)) # 0.999 value will be filled in with black color in the plot
+  rownames(zpI)[nrow(zpI)]<-"covsp"
+  
+  # for posnN
+  zpN<-z$posnN
+  zpN<-rbind(zpN,zcov[6,])
+  zpN<-cbind(zpN,matrix(0.999,nrow=nrow(zpN),ncol=tsp)) # 0.999 value will be filled in with black color in the plot
+  rownames(zpN)[nrow(zpN)]<-"covsp"
+  
+  # for corval
+  zcval<-z$corval
+  zcval<-rbind(zcval,zcov[7,])
+  zcval<-cbind(zcval,matrix(0.999,nrow=nrow(zcval),ncol=tsp)) # 0.999 value will be filled in with black color in the plot
+  rownames(zcval)[nrow(zcval)]<-"covsp"
+  
+  # for cells to not show in the plot
+  posn_notneeded<-which(zs==0.999,arr.ind=T)
+  
+  zres<-list(spear=zs,
+             kend=zk,
+             Corl=zcl,
+             Coru=zcu,
+             posnI=zpI,
+             posnN=zpN,
+             corval=zcval,
+             posn_notneeded=posn_notneeded)
+  saveRDS(zres,paste(resloc,"NonParamStat.RDS",sep=""))
+  
+  NonParamStat_matrixplot(data=zres,
+                          resloc=resloc,
+                          posn_notneeded=posn_notneeded,
+                          tot_target_sp=tot_target_sp,
+                          tl.cex=1.2,cl.cex=2,line=1)
+}
--- terrestrial.R
+++ terrestrial.R
@@ -57,15 +57,15 @@ for(i in 1:length(terres_study_id)){
   year<-xmat$YEAR
   xmat<-as.matrix(xmat[,-1])
   rownames(xmat)<-year
-  xlist<-split(x,x$Species)
-  xlist<-purrr::map(xlist,~ (.x %>% select(YEAR,Value)))
+ # xlist<-split(x,x$Species)
+ # xlist<-purrr::map(xlist,~ (.x %>% select(YEAR,Value)))
   
   xmeta<-xxm%>%filter(STUDY_ID==terres_study_id[i])
   
-  input_sp<-list(spmat=xmat,splist=xlist,meta=xmeta)
+  input_sp<-list(spmat=xmat,meta=xmeta)
   
   resloc2<-paste(resloc,terres_study_id[i],sep="")
-  saveRDS(input_sp,paste(resloc2,"/spmat_and_list.RDS",sep=""))
+  saveRDS(input_sp,paste(resloc2,"/spmat.RDS",sep=""))
 }
 
 #====================== now do the tail association analysis ===================
@@ -74,7 +74,7 @@ all_raresp_site_id<-c() # initiate to store bad sites with all rare sp.
 #----------- first save the input for tail analysis ---------------
 for(i in 1:length(terres_study_id)){
   siteid<-terres_study_id[i]
-  m<-readRDS(paste("./Results/Terrestrial/",siteid,"/spmat_and_list.RDS",sep=""))
+  m<-readRDS(paste("./Results/Terrestrial/",siteid,"/spmat.RDS",sep=""))
   
   # first we aggregated the rare sp (present even less than 30% of sampled years) into a pseudo sp 
   presentyr<-apply(X=m$spmat,MARGIN=2,FUN=function(x){sum(x>0)})
@@ -89,11 +89,8 @@ for(i in 1:length(terres_study_id)){
       raresp<-as.matrix(raresp) # this line is for when you have only one rare sp
       raresp<-apply(X=raresp,MARGIN=1,FUN=sum)
       m1<-m$spmat[,-rareid]
-      tot_target_sp<-ncol(m1)
       m1<-cbind(m1,raresp=raresp)
       m1<-as.data.frame(m1)
-      ms1<-m$splist[-rareid]
-      ms1$raresp<-data.frame(YEAR=ms1[[1]]$YEAR,mean_estimate=m1$raresp)
       
       #------- exclude ties having more than 80% of same values ----------
       #Ties<-apply(MARGIN=2,X=m1,FUN=function(x){length(x) - length(unique(x))})
@@ -104,12 +101,11 @@ for(i in 1:length(terres_study_id)){
       #  ms1<-ms1[-excludeTies]
       #}
       #--------------------------------------------------
-      input_tailanal<-list(m_df=m1,mlist=ms1,tot_target_sp=tot_target_sp)
+      input_tailanal<-m1
       
     }else{
       m1<-m$spmat
-      ms1<-m$splist
-      tot_target_sp<-ncol(m1)
+      
       #------- exclude ties having more than 50% of same values ----------
       #Ties<-apply(MARGIN=2,X=m1,FUN=function(x){length(x) - length(unique(x))})
       #excludeTies<-which(Ties>=0.8*nrow(m1)) # more than 80% ties are excluded
@@ -117,7 +113,7 @@ for(i in 1:length(terres_study_id)){
       #  m1<-m1[,-excludeTies]
       # ms1<-ms1[-excludeTies]
       #}
-      input_tailanal<-list(m_df=m1,mlist=ms1,tot_target_sp=tot_target_sp)
+      input_tailanal<-m1
     }
     
     saveRDS(input_tailanal,paste("./Results/Terrestrial/",siteid,"/input_tailanal.RDS",sep=""))
@@ -129,90 +125,16 @@ for(i in 1:length(terres_study_id)){
 terres_study_id<-setdiff(terres_study_id,all_raresp_site_id) 
 
 #------------ Now compute and plot the tail stats ---------------------
-source("./NonParamStat.R")
-source("./NonParamStat_matrixplot.R")
-source("./copula_covary.R")
+source("./tail_analysis.R")
 
 for(i in 1:length(terres_study_id)){
   siteid<-terres_study_id[i]
   resloc<-paste("./Results/Terrestrial/",siteid,"/",sep="")
-  d<-readRDS(paste(resloc,"input_tailanal.RDS",sep=""))
-  tot_target_sp<-d$tot_target_sp
-  
-  #------ analysis with species only ---------
-  d_allsp<-d$mlist
-  z<-multcall(d_allsp = d_allsp,resloc=resloc,nbin=2)
+  df<-readRDS(paste(resloc,"input_tailanal.RDS",sep="")) # dataframe with species timeseries along column
   
   #----------- analysis with covary sp ----------------
-  df<-d$m_df # dataframe with species timeseries along column
-  zcov<-copula_covary(df = df, nbin = 2)
-  
-  #----- now combine the results ------------
-  
-  # for spearman
-  zs<-z$spear
-  tsp<-ncol(zs)-ncol(zcov)
-  tempo<-matrix(0.999,nrow=nrow(zcov),ncol=tsp) # 0.999 value will be filled in with black color in the plot
-  zcov<-cbind(zcov,tempo)
-  zs<-rbind(zs,zcov[1,])
-  zs<-cbind(zs,matrix(0.999,nrow=nrow(zs),ncol=tsp)) # 0.999 value will be filled in with black color in the plot
-  rownames(zs)[nrow(zs)]<-"covsp"
-  
-  # for kend
-  zk<-z$kend
-  zk<-rbind(zk,zcov[2,])
-  zk<-cbind(zk,matrix(0.999,nrow=nrow(zk),ncol=tsp)) # 0.999 value will be filled in with black color in the plot
-  rownames(zk)[nrow(zk)]<-"covsp"
-  
-  # for Corl
-  zcl<-z$Corl
-  zcl<-rbind(zcl,zcov[3,])
-  zcl<-cbind(zcl,matrix(0.999,nrow=nrow(zcl),ncol=tsp)) # 0.999 value will be filled in with black color in the plot
-  rownames(zcl)[nrow(zcl)]<-"covsp"
-  
-  # for Coru
-  zcu<-z$Coru
-  zcu<-rbind(zcu,zcov[4,])
-  zcu<-cbind(zcu,matrix(0.999,nrow=nrow(zcu),ncol=tsp)) # 0.999 value will be filled in with black color in the plot
-  rownames(zcu)[nrow(zcu)]<-"covsp"
-  
-  # for posnI
-  zpI<-z$posnI
-  zpI<-rbind(zpI,zcov[5,])
-  zpI<-cbind(zpI,matrix(0.999,nrow=nrow(zpI),ncol=tsp)) # 0.999 value will be filled in with black color in the plot
-  rownames(zpI)[nrow(zpI)]<-"covsp"
-  
-  # for posnN
-  zpN<-z$posnN
-  zpN<-rbind(zpN,zcov[6,])
-  zpN<-cbind(zpN,matrix(0.999,nrow=nrow(zpN),ncol=tsp)) # 0.999 value will be filled in with black color in the plot
-  rownames(zpN)[nrow(zpN)]<-"covsp"
-  
-  # for corval
-  zcval<-z$corval
-  zcval<-rbind(zcval,zcov[7,])
-  zcval<-cbind(zcval,matrix(0.999,nrow=nrow(zcval),ncol=tsp)) # 0.999 value will be filled in with black color in the plot
-  rownames(zcval)[nrow(zcval)]<-"covsp"
-  
-  # for cells to not show in the plot
-  posn_notneeded<-which(zs==0.999,arr.ind=T)
-  
-  zres<-list(spear=zs,
-             kend=zk,
-             Corl=zcl,
-             Coru=zcu,
-             posnI=zpI,
-             posnN=zpN,
-             corval=zcval,
-             posn_notneeded=posn_notneeded)
-  saveRDS(zres,paste(resloc,"NonParamStat.RDS",sep=""))
-  NonParamStat_matrixplot(data=zres,
-                          resloc=resloc,
-                          posn_notneeded=posn_notneeded,
-                          tot_target_sp=tot_target_sp,
-                          tl.cex=1.2,cl.cex=2,line=1)
+  res<-tail_analysis(mat = df, resloc = resloc, nbin = 2)
 }
-
 
 #================ get a map for selecting Terrestrial sites ==========================
 
