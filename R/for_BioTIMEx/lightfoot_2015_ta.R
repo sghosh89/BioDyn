fd5271b5f87514eef742a60e28db9642e846cf45
sghosh89 <ghoshshyamolina89@gmail.com>
2021-01-17 23:02:04 +0100

    edits

---
+++ R/for_BioTIMEx/lightfoot_2015_ta.R
@@ -0,0 +1,56 @@
+# This script will do the copula analysis, will generate necessary plots, will show how a species 
+# tail dep. fluctuates with the rest of species in the phyto-plankton community
+
+source("tail_analysis.R")
+library(tidyverse)
+`%notin%` <- Negate(`%in%`)
+
+resloc<-'../../Results/for_BioTIMEx/lightfoot_2015/'
+if(!dir.exists(resloc)){
+  dir.create(resloc)
+}
+
+########################
+dataset_id<-"lightfoot_2015_site_BOER_sampling_time_E"
+myresloc<-paste(resloc,dataset_id,"/",sep="")
+if(!dir.exists(myresloc)){
+  dir.create(myresloc)
+}
+spmat<-readRDS(paste("../../DATA/for_BioTIMEx/wrangled_data/lightfoot_2015/lightfoot_2015_site_BOER_sampling_time_E_inputmatrix_tailanal.RDS",sep=""))
+
+tail_analysis(mat=spmat,resloc=myresloc,nbin=2)
+
+###########################
+dataset_id<-"lightfoot_2015_site_BOER_sampling_time_L"
+myresloc<-paste(resloc,dataset_id,"/",sep="")
+if(!dir.exists(myresloc)){
+  dir.create(myresloc)
+}
+spmat<-readRDS(paste("../../DATA/for_BioTIMEx/wrangled_data/lightfoot_2015/lightfoot_2015_site_BOER_sampling_time_L_inputmatrix_tailanal.RDS",sep=""))
+
+tail_analysis(mat=spmat,resloc=myresloc,nbin=2)
+
+########################
+dataset_id<-"lightfoot_2015_site_LATR_sampling_time_E"
+myresloc<-paste(resloc,dataset_id,"/",sep="")
+if(!dir.exists(myresloc)){
+  dir.create(myresloc)
+}
+spmat<-readRDS(paste("../../DATA/for_BioTIMEx/wrangled_data/lightfoot_2015/lightfoot_2015_site_LATR_sampling_time_E_inputmatrix_tailanal.RDS",sep=""))
+
+tail_analysis(mat=spmat,resloc=myresloc,nbin=2)
+
+###########################
+dataset_id<-"lightfoot_2015_site_LATR_sampling_time_L"
+myresloc<-paste(resloc,dataset_id,"/",sep="")
+if(!dir.exists(myresloc)){
+  dir.create(myresloc)
+}
+spmat<-readRDS(paste("../../DATA/for_BioTIMEx/wrangled_data/lightfoot_2015/lightfoot_2015_site_LATR_sampling_time_L_inputmatrix_tailanal.RDS",sep=""))
+
+tail_analysis(mat=spmat,resloc=myresloc,nbin=2)
+
+
+
+
+
