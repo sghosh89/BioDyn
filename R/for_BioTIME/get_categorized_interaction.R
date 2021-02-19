5eac1a68bda731ee5a2eae64f5dd70e3c02eedba
sghosh89 <ghoshshyamolina89@gmail.com>
2021-02-19 18:51:18 +0100

    rank based analysis for target sp added

---
+++ R/for_BioTIME/get_categorized_interaction.R
@@ -0,0 +1,187 @@
+# function to get interaction type (indep, synchrony, compensatory) between two species 
+# categorized into top 4 category based on rank abundance
+# Input:
+# x = output from NonParamStat function
+# xcat = output from get_rankabun function: a dataframe having target species name and category type: 1,2,3,4
+
+library(tidyverse)
+get_categorized_interaction<-function(x,xcat){
+  xcat$targetsp<-rownames(xcat)
+  xcat<-xcat[,c("targetsp","category")]
+  CorlmCoru<-x$Corl - x$Coru
+  #colnames(CorlmCoru)<-rownames(CorlmCoru) # when no raresp still covsp could be present then nrow>ncol
+  
+  numtargetsp<-rownames(xcat)
+  numtargetsp<-setdiff(numtargetsp,c("raresp","covsp"))
+  numtargetsp<-length(numtargetsp)
+  
+  # for indep. interaction pair
+  indepmat<-which(x$posnI==1,arr.ind = T)
+  if(nrow(indepmat)>0){
+    indepmat<-as.data.frame(indepmat)
+    indepmat$rowsp<-rownames(CorlmCoru)[indepmat$row]
+    indepmat$colsp<-rownames(CorlmCoru)[indepmat$col] # rownames and colnames are same for individual sp
+    badid<-which(indepmat$rowsp%in%c("raresp","covsp"))
+    if(length(badid)>0){indepmat<-indepmat[-badid,]}
+    badid<-which(indepmat$colsp%in%c("ra"))
+    if(length(badid)>0){indepmat<-indepmat[-badid,]}
+    indepmat<-left_join(indepmat,xcat,by=c("rowsp"="targetsp"))
+    indepmat<-left_join(indepmat,xcat,by=c("colsp"="targetsp"))
+    colnames(indepmat)[5:6]<-c("rowsp_category","colsp_category")
+    indepmat$interaction_category<-paste(indepmat$rowsp_category,indepmat$colsp_category,sep="_")
+    indepmat$interaction_category[which(indepmat$interaction_category=="2_1")]<-"1_2"
+    indepmat$interaction_category[which(indepmat$interaction_category=="3_1")]<-"1_3"
+    indepmat$interaction_category[which(indepmat$interaction_category=="4_1")]<-"1_4"
+    indepmat$interaction_category[which(indepmat$interaction_category=="3_2")]<-"2_3"
+    indepmat$interaction_category[which(indepmat$interaction_category=="4_2")]<-"2_4"
+    indepmat$interaction_category[which(indepmat$interaction_category=="4_3")]<-"3_4"
+  }else{
+    indepmat<-NA
+  }
+  #------------- save indepmat for indep. interaction category info -----------------
+  
+  # for synchronous interaction
+  posnI<-which(x$posnI==1,arr.ind = T)
+  posnN<-which(x$posnN==1,arr.ind = T)
+  z<-CorlmCoru
+  z[posnI]<-NA
+  if(nrow(posnN)>0){z[posnN]<-NA}
+  synmat<-which(z!=0,arr.ind = T)
+  if(nrow(synmat)>0){ # if there is any synchronous dynamics
+    synmat<-as.data.frame(synmat)
+    synmat$rowsp<-rownames(CorlmCoru)[synmat$row]
+    synmat$colsp<-rownames(CorlmCoru)[synmat$col]
+    badid<-which(synmat$rowsp%in%c("raresp","covsp"))
+    if(length(badid)>0){synmat<-synmat[-badid,]}
+    badid<-which(synmat$colsp%in%c("raresp","covsp"))
+    if(length(badid)>0){synmat<-synmat[-badid,]}
+    synmat<-left_join(synmat,xcat,by=c("rowsp"="targetsp"))
+    synmat<-left_join(synmat,xcat,by=c("colsp"="targetsp"))
+    colnames(synmat)[5:6]<-c("rowsp_category","colsp_category")
+    synmat$LTmUT<-z[as.matrix(synmat[,1:2])]
+    synmat$type<-ifelse(synmat$LTmUT>0,"LT","UT")
+    synmat$interaction_category<-paste(synmat$rowsp_category,synmat$colsp_category,sep="_")
+    synmat$interaction_category[which(synmat$interaction_category=="2_1")]<-"1_2"
+    synmat$interaction_category[which(synmat$interaction_category=="3_1")]<-"1_3"
+    synmat$interaction_category[which(synmat$interaction_category=="4_1")]<-"1_4"
+    synmat$interaction_category[which(synmat$interaction_category=="3_2")]<-"2_3"
+    synmat$interaction_category[which(synmat$interaction_category=="4_2")]<-"2_4"
+    synmat$interaction_category[which(synmat$interaction_category=="4_3")]<-"3_4"
+    #barplot((table(synmat$interaction_category)/nrow(synmat)),ylim=c(0,1))
+    #synmat_LT<-synmat%>%filter(type=="LT")
+    #barplot((table(synmat_LT$interaction_category)/nrow(synmat)),col="red")
+    #synmat_UT<-synmat%>%filter(type=="UT")
+    #barplot((table(synmat_UT$interaction_category)/nrow(synmat)),col="blue")
+  }else{
+    synmat<-NA
+  }
+  #------------- save synmat for synchronous interaction category info -----------------
+  
+  # for compensatory interaction
+  compmat<-which(x$posnN==1,arr.ind = T)
+  if(nrow(compmat)>0){ # if there is any compensatory dynamics
+    compmat<-as.data.frame(compmat)
+    compmat$rowsp<-rownames(CorlmCoru)[compmat$row]
+    compmat$colsp<-rownames(CorlmCoru)[compmat$col]
+    badid<-which(compmat$rowsp%in%c("raresp","covsp"))
+    if(length(badid)>0){compmat<-compmat[-badid,]}
+    badid<-which(compmat$colsp%in%c("raresp","covsp"))
+    if(length(badid)>0){compmat<-compmat[-badid,]}
+    compmat<-left_join(compmat,xcat,by=c("rowsp"="targetsp"))
+    compmat<-left_join(compmat,xcat,by=c("colsp"="targetsp"))
+    colnames(compmat)[5:6]<-c("rowsp_category","colsp_category")
+    compmat$LTmUT<-CorlmCoru[as.matrix(compmat[,1:2])]
+    compmat$strongcomp_sp<-ifelse(compmat$LTmUT>0,compmat$colsp,compmat$rowsp)
+    compmat$strongcomp_category<-ifelse(compmat$LTmUT>0,compmat$colsp_category,compmat$rowsp_category)
+    compmat$interaction_category<-paste(compmat$rowsp_category,compmat$colsp_category,sep="_")
+    compmat$interaction_category[which(compmat$interaction_category=="2_1")]<-"1_2"
+    compmat$interaction_category[which(compmat$interaction_category=="3_1")]<-"1_3"
+    compmat$interaction_category[which(compmat$interaction_category=="4_1")]<-"1_4"
+    compmat$interaction_category[which(compmat$interaction_category=="3_2")]<-"2_3"
+    compmat$interaction_category[which(compmat$interaction_category=="4_2")]<-"2_4"
+    compmat$interaction_category[which(compmat$interaction_category=="4_3")]<-"3_4"
+  }else{
+    compmat<-NA
+  }
+  
+  # barplot(table(compmat$interaction_category)/nrow(compmat))
+  #------------- save compmat for compensatory interaction category info -----------------
+  
+  interaction_type<-c("1_1","1_2","1_3","1_4","2_2","2_3","2_4","3_3","3_4","4_4")
+  table_indep<-as.data.frame(matrix(NA,nrow=1,ncol=10))
+  colnames(table_indep)<-interaction_type
+  table_syn<-table_indep
+  table_comp<-table_indep
+  if(is.data.frame(indepmat)){
+    table_indep$`1_1`<-sum(indepmat$interaction_category=="1_1")
+    table_indep$`1_2`<-sum(indepmat$interaction_category=="1_2")
+    table_indep$`1_3`<-sum( indepmat$interaction_category=="1_3")
+    table_indep$`1_4`<-sum( indepmat$interaction_category=="1_4")
+    table_indep$`2_2`<-sum( indepmat$interaction_category=="2_2")
+    table_indep$`2_3`<-sum( indepmat$interaction_category=="2_3")
+    table_indep$`2_4`<-sum( indepmat$interaction_category=="2_4")
+    table_indep$`3_3`<-sum( indepmat$interaction_category=="3_3")
+    table_indep$`3_4`<-sum( indepmat$interaction_category=="3_4")
+    table_indep$`4_4`<-sum( indepmat$interaction_category=="4_4")
+  }
+  
+  if(is.data.frame(synmat)){
+    table_syn$`1_1`<-sum( synmat$interaction_category=="1_1")
+    table_syn$`1_2`<-sum( synmat$interaction_category=="1_2")
+    table_syn$`1_3`<-sum( synmat$interaction_category=="1_3")
+    table_syn$`1_4`<-sum( synmat$interaction_category=="1_4")
+    table_syn$`2_2`<-sum( synmat$interaction_category=="2_2")
+    table_syn$`2_3`<-sum( synmat$interaction_category=="2_3")
+    table_syn$`2_4`<-sum( synmat$interaction_category=="2_4")
+    table_syn$`3_3`<-sum( synmat$interaction_category=="3_3")
+    table_syn$`3_4`<-sum( synmat$interaction_category=="3_4")
+    table_syn$`4_4`<-sum( synmat$interaction_category=="4_4")
+  }
+  
+  if(is.data.frame(compmat)){
+    table_comp$`1_1`<-sum( compmat$interaction_category=="1_1")
+    table_comp$`1_2`<-sum( compmat$interaction_category=="1_2")
+    table_comp$`1_3`<-sum( compmat$interaction_category=="1_3")
+    table_comp$`1_4`<-sum( compmat$interaction_category=="1_4")
+    table_comp$`2_2`<-sum( compmat$interaction_category=="2_2")
+    table_comp$`2_3`<-sum( compmat$interaction_category=="2_3")
+    table_comp$`2_4`<-sum( compmat$interaction_category=="2_4")
+    table_comp$`3_3`<-sum( compmat$interaction_category=="3_3")
+    table_comp$`3_4`<-sum( compmat$interaction_category=="3_4")
+    table_comp$`4_4`<-sum( compmat$interaction_category=="4_4")
+  }
+  
+  table_indep<-t(table_indep)
+  table_syn<-t(table_syn)
+  table_comp<-t(table_comp)
+  
+  table_interaction<-cbind(table_indep,table_syn,table_comp)
+  colnames(table_interaction)<-colnames(freq_interaction)<-c("indep","syn","comp")
+  table_interaction<-as.data.frame(table_interaction)
+  
+  table_interaction$poss_interaction<-NA
+  tt<-as.data.frame(table(xcat$category))
+  table_interaction$poss_interaction[1]<-tt$Freq[1]*tt$Freq[1] 
+  table_interaction$poss_interaction[2]<-tt$Freq[1]*tt$Freq[2] 
+  table_interaction$poss_interaction[3]<-tt$Freq[1]*tt$Freq[3]
+  table_interaction$poss_interaction[4]<-tt$Freq[1]*tt$Freq[4]
+  table_interaction$poss_interaction[5]<-tt$Freq[2]*tt$Freq[2] 
+  table_interaction$poss_interaction[6]<-tt$Freq[2]*tt$Freq[3]
+  table_interaction$poss_interaction[7]<-tt$Freq[2]*tt$Freq[4]
+  table_interaction$poss_interaction[8]<-tt$Freq[3]*tt$Freq[3]
+  table_interaction$poss_interaction[9]<-tt$Freq[3]*tt$Freq[4]
+  table_interaction$poss_interaction[10]<-tt$Freq[4]*tt$Freq[4]
+  table_interaction$freq_indep<-table_interaction$indep/table_interaction$poss_interaction
+  table_interaction$freq_syn<-table_interaction$syn/table_interaction$poss_interaction
+  table_interaction$freq_comp<-table_interaction$comp/table_interaction$poss_interaction
+
+  res<-list(numtargetsp=numtargetsp,
+            indepmat=indepmat,
+            synmat=synmat,
+            compmat=compmat,
+            table_interaction=table_interaction)
+  
+  return(res)
+}
+
+
---
+++ R/for_BioTIME/rank_category.R
@@ -0,0 +1,163 @@
+library(BiodiversityR)
+library(tidyverse)
+library(gridExtra)
+#--------------------------------
+# The following function will take the input matrix for tail analysis and tag the species
+# into top 4 category based on their rank abundance
+
+get_rankabun<-function(siteid,realm){
+  for(i in 1:length(siteid)){
+    site<-siteid[i]
+    i_mat<-readRDS(paste("../../Results/for_BioTIME/",realm,"/",site,"/input_tailanal.RDS",sep=""))
+    if(tail(colnames(i_mat),1)=="raresp"){
+      i_mat<-i_mat[,-ncol(i_mat)]
+    }
+    rankabun <- rankabundance(i_mat)
+    rankabun<-as.data.frame(rankabun)
+    qs<-quantile(rankabun$abundance) # 0,0.25,0.5,0.75,1
+    # category: top four: 1: within 75-100%, 2: within 50-75%, 3: within 25-50%, 4: within 0-25%
+    rankabun$category<-NA
+    qs<-unname(qs)
+    for(j in 1:nrow(rankabun)){
+      r<-rankabun$abundance[j]
+      if(r<qs[2]){
+        rankabun$category[j]<-4
+      }else if(r>=qs[2] & r<qs[3]){
+        rankabun$category[j]<-3
+      }else if(r>=qs[3] & r<qs[4]){
+        rankabun$category[j]<-2
+      }else{
+        rankabun$category[j]<-1
+      }
+    }
+    
+    saveRDS(rankabun,paste("../../Results/for_BioTIME/",realm,"/",site,"/rankabun.RDS",sep=""))
+    
+  }
+}
+
+#########################################
+# now call the function for freshwater
+r_frs<-readRDS("../../Results/for_BioTIME/Freshwater/summary_table.RDS")
+siteid<-r_frs$siteid
+realm<-"Freshwater"
+get_rankabun(siteid = siteid,realm = realm)
+
+# now call the function for marine
+r_mar<-readRDS("../../Results/for_BioTIME/Marine/summary_table.RDS")
+siteid<-r_mar$siteid
+realm<-"Marine"
+get_rankabun(siteid = siteid,realm = realm)
+
+# now call the function for terrestrial
+r_ter<-readRDS("../../Results/for_BioTIME/Terrestrial/summary_table.RDS")
+siteid<-r_ter$siteid
+realm<-"Terrestrial"
+get_rankabun(siteid = siteid,realm = realm)
+
+#==================================================================================
+source("./get_categorized_interaction.R")
+
+# for freshwater
+mypath<-"../../Results/for_BioTIME/Freshwater/"
+siteid<-r_frs$siteid
+for(i in 1:length(siteid)){
+  x<-readRDS(paste(mypath,"/",siteid[i],"/NonParamStat.RDS",sep=""))
+  xcat<-readRDS(paste(mypath,"/",siteid[i],"/rankabun.RDS",sep=""))
+  res<-get_categorized_interaction(x=x,xcat=xcat)
+  cat("i = ",i,"\n")
+  saveRDS(res,paste(mypath,"/",siteid[i],"/categorized_interaction.RDS",sep=""))
+}
+
+# for marine
+mypath<-"../../Results/for_BioTIME/Marine/"
+siteid<-r_mar$siteid
+for(i in 1:length(siteid)){
+  x<-readRDS(paste(mypath,"/",siteid[i],"/NonParamStat.RDS",sep=""))
+  xcat<-readRDS(paste(mypath,"/",siteid[i],"/rankabun.RDS",sep=""))
+  res<-get_categorized_interaction(x=x,xcat=xcat)
+  cat("i = ",i,"\n")
+  saveRDS(res,paste(mypath,"/",siteid[i],"/categorized_interaction.RDS",sep=""))
+}
+
+# for terrestrial
+mypath<-"../../Results/for_BioTIME/Terrestrial/"
+siteid<-r_ter$siteid
+for(i in 1:length(siteid)){
+  x<-readRDS(paste(mypath,"/",siteid[i],"/NonParamStat.RDS",sep=""))
+  xcat<-readRDS(paste(mypath,"/",siteid[i],"/rankabun.RDS",sep=""))
+  res<-get_categorized_interaction(x=x,xcat=xcat)
+  cat("i = ",i,"\n")
+  saveRDS(res,paste(mypath,"/",siteid[i],"/categorized_interaction.RDS",sep=""))
+}
+#########################################################################################
+# plot the results 
+
+# for freshwater
+mypath<-"../../Results/for_BioTIME/Freshwater/"
+siteid<-r_frs$siteid
+res<-c()
+for(i in 1:length(siteid)){
+  x<-readRDS(paste(mypath,"/",siteid[i],"/categorized_interaction.RDS",sep=""))
+  d<-x$table_interaction
+  d<-d[,5:7]
+  d$Interaction<-rownames(d)
+  res<-rbind(res,d)
+}
+rownames(res)<-NULL
+res_freshw<-gather(res,key="freq_type",value = "Frequency",freq_indep,freq_syn,freq_comp)
+
+# for marine
+mypath<-"../../Results/for_BioTIME/Marine/"
+siteid<-r_mar$siteid
+res<-c()
+for(i in 1:length(siteid)){
+  x<-readRDS(paste(mypath,"/",siteid[i],"/categorized_interaction.RDS",sep=""))
+  d<-x$table_interaction
+  d<-d[,5:7]
+  d$Interaction<-rownames(d)
+  res<-rbind(res,d)
+}
+rownames(res)<-NULL
+res_marine<-gather(res,key="freq_type",value = "Frequency",freq_indep,freq_syn,freq_comp)
+
+# for terrestrial
+mypath<-"../../Results/for_BioTIME/Terrestrial/"
+siteid<-r_ter$siteid
+res<-c()
+for(i in 1:length(siteid)){
+  x<-readRDS(paste(mypath,"/",siteid[i],"/categorized_interaction.RDS",sep=""))
+  d<-x$table_interaction
+  d<-d[,5:7]
+  d$Interaction<-rownames(d)
+  res<-rbind(res,d)
+}
+rownames(res)<-NULL
+res_terres<-gather(res,key="freq_type",value = "Frequency",freq_indep,freq_syn,freq_comp)
+
+pdf("../../Results/for_BioTIME/interaction_freq_for_eachrealm.pdf",height=8,width=16)
+
+freshw<-ggplot(data=res_freshw,aes(x=Interaction,y=Frequency,col=freq_type))+geom_point(pch=19)+ 
+  scale_color_manual(values=c("green", "gold1", "red"),guide=F)+
+  theme_bw()
+
+marine<-ggplot(data=res_marine,aes(x=Interaction,y=Frequency,col=freq_type))+geom_point(pch=19)+ 
+  scale_color_manual(values=c("green", "gold1", "red"),guide=F)+
+  theme_bw()
+
+terres<-ggplot(data=res_terres,aes(x=Interaction,y=Frequency,col=freq_type))+geom_point(pch=19)+ 
+  scale_color_manual(values=c("green", "gold1", "red"))+
+  theme_bw()
+
+grid.arrange(freshw,marine,terres, ncol=2, nrow=2)
+
+dev.off()
+
+
+
+
+
+
+
+
+
