a9477906c72bd192b36e2199b45fbe1b94a560c7
sghosh89 <ghoshshyamolina89@gmail.com>
2021-04-10 18:26:09 +0200

    basic univar bayesian model with conditional plot, constant intercept

---
+++ R/gather_res/compare_model_brms.R
@@ -0,0 +1,187 @@
+# This script will compare relationships between response (stability) 
+# and its predictors (richness, var ratio, skew ratio, asymmetry, dir of asymmetry)
+# between two realms: freshwater, terrestrial
+# basic Bayesian modeling is used with constant intercept
+#===========================================================
+rm(list=ls())
+library(brms)
+library(tidyverse)
+#========================================================================================
+sink("./console_brms_univar_model.txt", append=TRUE, split=TRUE)
+#====================================================================================================
+sm_all<-readRDS("../../Results/gather_res/stability_metric_all.RDS")
+
+# exclude marine realm
+sm_all$REALM<-as.character(sm_all$REALM)
+sm_all<-sm_all%>%filter(REALM%in%c("Freshwater","Terrestrial"))
+sm_all$REALM<-as.factor(sm_all$REALM)
+
+mydat<-sm_all[,c("siteid","iCV","phi","REALM","phi_skw","nsp","L","U")]
+mydat$A<-mydat$L+abs(mydat$U) # total asymmetry
+mydat$uniA<-mydat$L+mydat$U
+mydat$community<-"NT" #Symmetric
+mydat$community[which(mydat$L>abs(mydat$U))]<-"LT"
+mydat$community[which(mydat$L<abs(mydat$U))]<-"UT"
+#mydat<-mydat%>%filter(community%in%c("LT","UT"))
+
+mydat<-mydat%>%rename(
+  stability=iCV,
+  VR=phi,
+  SR=phi_skw,
+  R=nsp)
+mydat<-na.omit(mydat) # one community G8554 has dominant target sp. varying indep with each other, so 
+# SR was not calculated - VR showing compensatory dynamics (<1) due to other common sp not considered
+# in tail-analysis.
+
+#==============================================================================
+# first, we should scale the variables we are going to consider
+# scaled quantity = (actual quantity -mean(actual quantity))/sd(actual quantity)
+mydat<-mydat%>%select(stability,R,VR,SR,A,uniA,REALM)
+mydat_scaled<-mydat
+mydat_scaled$stability<-scale(mydat_scaled$stability)
+mydat_scaled$R<-scale(mydat_scaled$R)
+mydat_scaled$VR<- scale(mydat_scaled$VR) 
+mydat_scaled$SR<- scale(mydat_scaled$SR)
+mydat_scaled$A<- scale(mydat_scaled$A)
+mydat_scaled$uniA<- scale(mydat_scaled$uniA)
+#===============================================================================
+
+#----------- is there any difference between null and full model, and also between habitats --------
+model1<-lm(stability ~ (R + VR)*REALM, mydat_scaled) # R:REALM + VR:REALM +REALM
+model2<-lm(stability ~ (R + VR + SR + A + uniA)*REALM, mydat_scaled)
+anova(model1,model2)
+AIC(model1,model2)
+#car::vif(stability ~ (R + VR), mydat_scaled) testing multicolinearity 
+#car::vif(stability ~ (R + VR+ SR + A + uniA), mydat_scaled) testing multicolinearity 
+
+#OK, model 2 including all vars is the best one
+
+#======================================================================================================
+
+#===============================================================================================
+cat(paste("========= starting null_model Bayesian at ",Sys.time()," ============= \n"))
+#===============================================================================================
+bf_stability0<-bf(stability ~ (R + VR )* REALM) 
+bform0 <- bf_stability0 
+
+null_model<-brm(bform0,
+                data=mydat_scaled,
+                chains=4,cores=4,iter=10000,
+                control = list(adapt_delta = 0.99, max_treedepth = 20),
+                save_pars = save_pars(all = TRUE),seed=123)
+print(summary(null_model),digits = 3)
+saveRDS(null_model,"../../Results/gather_res/null_moddel.RDS")
+
+cat(paste("========= starting a basic full_model Bayesian at ",Sys.time()," ============= \n"))
+
+#bf_stability<-bf(stability ~ (R + VR + SR + A + uniA )* REALM) 
+#bform <- bf_stability 
+
+#full_model<-brm(bform,
+#                data=mydat_scaled,
+#                chains=4,cores=4,iter=10000,
+#                control = list(adapt_delta = 0.99, max_treedepth = 20),
+#                save_pars = save_pars(all = TRUE),seed=123)
+
+full_model <- update(null_model, stability ~ (R + VR + SR + A + uniA )* REALM, 
+                     newdata = mydat_scaled)
+saveRDS(full_model,"../../Results/gather_res/full_moddel.RDS")
+print(summary(full_model),digits = 3)
+#plot(full_model)
+#plot(conditional_effects(full_model), points = T, ask=F)
+# fixef(full_model) #population level effect
+#full_model<-readRDS("../../Results/gather_res/full_moddel.RDS")
+Sys.time()
+
+#===============================================================================================
+cat(paste("========= finished model Bayesian at ",Sys.time()," ============= \n"))
+#===============================================================================================
+# compare stats: which model is better?
+cat("==== loo for null model =====","\n")
+(loo_null<-loo(null_model,moment_match = T,reloo = T))
+saveRDS(loo_null,"../../Results/gather_res/loo_null.RDS")
+cat("==== loo for full model =====","\n")
+(loo_full<-loo(full_model,moment_match = T,reloo = T))
+saveRDS(loo_full,"../../Results/gather_res/loo_full.RDS")
+
+cat("==== compare loo for both model =====","\n")
+loo_compare(loo_null,loo_full)
+loo_model_weights(null_model,full_model)
+
+cat("==== R2 for null model =====","\n")
+loo_R2(null_model)
+
+cat("==== R2 for full model =====","\n")
+loo_R2(full_model)
+sink()
+
+#========================================================
+plot(full_model)
+
+x<-conditional_effects(full_model
+                       #,method = "posterior_predict"
+                       )
+
+#plot conditional effects
+pdf("./plot.pdf",height=12,width=16)
+op<-par(mfrow=c(3,2),mar=c(3,3,3,3))
+
+p1<-plot(x,points=F,plot=F)[[1]]+theme_classic()
+
+p2<-plot(x,points=F,plot=F)[[2]]+theme_classic()
+
+p3<-plot(x,points=F,plot=F)[[3]]+theme_classic()
+
+p4<-plot(x,points=F,plot=F)[[4]]+theme_classic()
+
+p5<-plot(x,points=F,plot=F)[[5]]+theme_classic()
+
+p6<-plot(x,points=F,plot=F)[[6]]+theme_classic()+
+    scale_color_manual(values=c("skyblue","green"))+
+    scale_fill_manual(values=c("skyblue","green"))
+
+p7<-plot(x,points=F,plot=F)[[7]]+theme_classic()+
+  scale_color_manual(values=c("skyblue","green"))+
+  scale_fill_manual(values=c("skyblue","green"))+
+  theme(legend.position = "none")
+
+p8<-plot(x,points=F,plot=F)[[8]]+theme_classic()+
+  scale_color_manual(values=c("skyblue","green"))+
+  scale_fill_manual(values=c("skyblue","green"))+
+  theme(legend.position = "none")
+
+p9<-plot(x,points=F,plot=F)[[9]]+theme_classic()+
+  scale_color_manual(values=c("skyblue","green"))+
+  scale_fill_manual(values=c("skyblue","green"))+
+  theme(legend.position = "none")
+
+p10<-plot(x,points=F,plot=F)[[10]]+theme_classic()+
+  scale_color_manual(values=c("skyblue","green"))+
+  scale_fill_manual(values=c("skyblue","green"))+
+  theme(legend.position = "none")
+
+p11<-plot(x,points=F,plot=F)[[11]]+theme_classic()+
+  scale_color_manual(values=c("skyblue","green"))+
+  scale_fill_manual(values=c("skyblue","green"))
+
+gridExtra::grid.arrange(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11, nrow =3, ncol=4)
+par(op)
+dev.off()
+
+#============================
+post <- posterior_samples(full_model) # posterior distribution
+
+# level of support
+LOS <- function(x=NULL){
+  out =   (length(which(x > 0)) / length(x))*100
+  return(round(out, 3))
+}
+apply(post, 2, LOS)
+
+#============================
+
+#posterior_summary(full_model)
+#print(summary(full_model),digits=3)
+# conditional effects mean interaction between two var where other remains fixed(=0)
+
+
---
+++ R/gather_res/plotter_conditional_effect_brms_univar.R
@@ -0,0 +1,184 @@
+# For two realms we want to see conditional effects 
+library(brms)
+library(rethinking)
+
+# so first, we need to go back to raw var
+# mydat_scaled is scaled data, mydat is the raw data
+pred_func <- function(post, R, VR, SR, A, uniA, REALM){
+  stab <-  with(post, b_Intercept + b_R * R + b_VR*VR + b_SR*SR +
+                  b_A* A + b_uniA*uniA + b_REALMTerrestrial* REALM +
+                  `b_R:REALMTerrestrial` * REALM*R + `b_VR:REALMTerrestrial` * REALM*VR +
+                  `b_SR:REALMTerrestrial` * REALM*SR +  
+                  `b_A:REALMTerrestrial` * REALM*A + `b_uniA:REALMTerrestrial` * REALM*uniA)
+  
+  stab<-stab*(sd(stab))+mean(stab) # bring back raw var from scaled var
+}
+
+# plot conditional effects with original variables
+plotter_conditional_effect_brms_univar<-function(full_model,xvar,ylm,xlab="R",ploton="varyR"){
+  post <- posterior_samples(full_model) # posterior distribution
+  
+  #---------------------------- for Richness effect -------------------------
+  if(ploton=="varyR"){
+    # Terrestrial
+    T.pred = sapply(1:length(xvar), function(i) pred_func(post = post, R = xvar[i], SR = 0, A = 0,
+                                                          uniA = 0, VR = 0, REALM = 1  ))
+    #dim(T.pred)
+    meanT <- apply(T.pred, 2, mean)
+    ciT <- apply(T.pred, 2, HPDI, prob= .95)
+    
+    #Freshwater
+    F.pred = sapply(1:length(xvar), function(i) pred_func(post = post, R = xvar[i], SR = 0, A = 0,
+                                                          uniA = 0, VR = 0, REALM = 0  ))
+    #dim(F.pred)
+    meanF <- apply(F.pred, 2, mean)
+    ciF <- apply(F.pred, 2, HPDI, prob= .95)
+    
+    # now plotting the posterior for two realm
+    plot(xvar,meanT, pch="", ylim = ylm, xlab=xlab, ylab="Stability")
+    lines(xvar,meanT, col='green')
+    shade(ciT,xvar, col=alpha("green", 0.2))
+    
+    lines(xvar,meanF, col='skyblue')
+    shade(ciF,xvar, col=alpha("skyblue", 0.2))
+  }
+  
+  
+  #---------------------------- for Variance Ratio effect -------------------------
+  if(ploton=="varyVR"){
+    # Terrestrial
+    T.pred = sapply(1:length(xvar), function(i) pred_func(post = post, R = 0, SR = 0, A = 0,
+                                                          uniA = 0, VR = xvar[i], REALM = 1  ))
+    #dim(T.pred)
+    meanT <- apply(T.pred, 2, mean)
+    ciT <- apply(T.pred, 2, HPDI, prob= .95)
+    
+    #Freshwater
+    F.pred = sapply(1:length(xvar), function(i) pred_func(post = post, R = 0, SR = 0, A = 0,
+                                                          uniA = 0, VR = xvar[i], REALM = 0  ))
+    #dim(F.pred)
+    meanF <- apply(F.pred, 2, mean)
+    ciF <- apply(F.pred, 2, HPDI, prob= .95)
+    
+    # now plotting the posterior for two realm
+    plot(xvar,meanT, pch="", ylim = ylm, xlab=xlab, ylab="Stability")
+    lines(xvar,meanT, col='green')
+    shade(ciT,xvar, col=alpha("green", 0.2))
+    
+    lines(xvar,meanF, col='skyblue')
+    shade(ciF,xvar, col=alpha("skyblue", 0.2))
+  }
+  
+  #---------------------------- for Skewness Ratio effect -------------------------
+  if(ploton=="varySR"){
+    # Terrestrial
+    T.pred = sapply(1:length(xvar), function(i) pred_func(post = post, R = 0, SR = xvar[i], A = 0,
+                                                          uniA = 0, VR = 0, REALM = 1  ))
+    #dim(T.pred)
+    meanT <- apply(T.pred, 2, mean)
+    ciT <- apply(T.pred, 2, HPDI, prob= .95)
+    
+    #Freshwater
+    F.pred = sapply(1:length(xvar), function(i) pred_func(post = post, R = 0, SR = xvar[i], A = 0,
+                                                          uniA = 0, VR = 0, REALM = 0  ))
+    #dim(F.pred)
+    meanF <- apply(F.pred, 2, mean)
+    ciF <- apply(F.pred, 2, HPDI, prob= .95)
+    
+    # now plotting the posterior for two realm
+    plot(xvar,meanT, pch="", ylim = ylm, xlab=xlab, ylab="Stability")
+    lines(xvar,meanT, col='green')
+    shade(ciT,xvar, col=alpha("green", 0.2))
+    
+    lines(xvar,meanF, col='skyblue')
+    shade(ciF,xvar, col=alpha("skyblue", 0.2))
+  }
+  
+  #---------------------------- for Abs asymmetry effect -------------------------
+  if(ploton=="varyA"){
+    # Terrestrial
+    T.pred = sapply(1:length(xvar), function(i) pred_func(post = post, R = 0, SR = 0, A = xvar[i],
+                                                          uniA = 0, VR = 0, REALM = 1  ))
+    #dim(T.pred)
+    meanT <- apply(T.pred, 2, mean)
+    ciT <- apply(T.pred, 2, HPDI, prob= .95)
+    
+    #Freshwater
+    F.pred = sapply(1:length(xvar), function(i) pred_func(post = post, R =0, SR = 0, A = xvar[i],
+                                                          uniA = 0, VR = 0, REALM = 0  ))
+    #dim(F.pred)
+    meanF <- apply(F.pred, 2, mean)
+    ciF <- apply(F.pred, 2, HPDI, prob= .95)
+    
+    # now plotting the posterior for two realm
+    plot(xvar,meanT, pch="", ylim = ylm, xlab=xlab, ylab="Stability")
+    lines(xvar,meanT, col='green')
+    shade(ciT,xvar, col=alpha("green", 0.2))
+    
+    lines(xvar,meanF, col='skyblue')
+    shade(ciF,xvar, col=alpha("skyblue", 0.2))
+  }
+  
+  #---------------------------- for directional asymmetry effect -------------------------
+  if(ploton=="varyuniA"){
+    # Terrestrial
+    T.pred = sapply(1:length(xvar), function(i) pred_func(post = post, R = 0, SR = 0, A = 0,
+                                                          uniA = xvar[i], VR = 0, REALM = 1  ))
+    #dim(T.pred)
+    meanT <- apply(T.pred, 2, mean)
+    ciT <- apply(T.pred, 2, HPDI, prob= .95)
+    
+    #Freshwater
+    F.pred = sapply(1:length(xvar), function(i) pred_func(post = post, R = 0, SR = 0, A = 0,
+                                                          uniA =xvar[i], VR = 0, REALM = 0  ))
+    #dim(F.pred)
+    meanF <- apply(F.pred, 2, mean)
+    ciF <- apply(F.pred, 2, HPDI, prob= .95)
+    
+    # now plotting the posterior for two realm
+    plot(xvar,meanT, pch="", ylim = ylm, xlab=xlab, ylab="Stability")
+    lines(xvar,meanT, col='green')
+    shade(ciT,xvar, col=alpha("green", 0.2))
+    
+    lines(xvar,meanF, col='skyblue')
+    shade(ciF,xvar, col=alpha("skyblue", 0.2))
+  }
+  
+}
+
+#==============================================
+full_model<-readRDS("../../Results/gather_res/full_moddel.RDS")
+pdf("../../Results/gather_res/conditional_brms_univar_plot.pdf",height=3,width=6)
+op<-par(mfrow=c(2,3),mar=c(4,4,1,1),mgp=c(2.5,1,0))
+xvar = seq(from=2, to=70, by = 1)
+plotter_conditional_effect_brms_univar(full_model=full_model,xvar=xvar,
+                                       ylm = c(-100,180),
+                                       xlab = "Richness",
+                                       ploton="varyR")
+
+
+xvar = seq(from=0.01, to=10, by = 0.1)
+plotter_conditional_effect_brms_univar(full_model=full_model,xvar=xvar,
+                                       ylm = c(-4.5,1),
+                                       xlab = "Variance Ratio",
+                                       ploton="varyVR")
+
+xvar = seq(from=-10, to=10, by = 1)
+plotter_conditional_effect_brms_univar(full_model=full_model,xvar=xvar,
+                                       ylm = c(-2,1),
+                                       xlab = "Skewness Ratio",
+                                       ploton="varySR")
+xvar = seq(from=0, to=40, by = 1)
+plotter_conditional_effect_brms_univar(full_model=full_model,xvar=xvar,
+                                       ylm = c(-180,100),
+                                       xlab = "Total abs. asymmetry",
+                                       ploton="varyA")
+xvar = seq(from=-30, to=30, by = 1)
+plotter_conditional_effect_brms_univar(full_model=full_model,xvar=xvar,
+                                       ylm = c(-42,40),
+                                       xlab = "Net asymmetry",
+                                       ploton="varyuniA")
+
+par(op)
+dev.off()
+
