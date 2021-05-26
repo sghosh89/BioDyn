aa978c7218b77053f21dda0d7640988673dd9193
sghosh89 <ghoshshyamolina89@gmail.com>
2021-05-26 18:30:02 +0200

    summary started

--- R/for_BioTIME/terrestrial_plotlevel.R
+++ R/for_BioTIME/terrestrial_plotlevel.R
@@ -109,6 +109,80 @@ df<-readRDS("../../DATA/for_BioTIME/wrangled_data/Terrestrial_plotlevel/table_fo
 df_included<-df%>%filter(site%notin%c(195,221,298,300,356,360,414,416,483,497))
 saveRDS(df_included,"../../DATA/for_BioTIME/wrangled_data/Terrestrial_plotlevel/table_for_map_selected.RDS")
 
+#--------------- Do a summary stats for terrestrial sites ------------------
+summary_table<-c()
+for (i in c(1:length(df_included$site))){
+  resloc<-paste("../../DATA/for_BioTIME/wrangled_data/Terrestrial_plotlevel/",df_included$site[i],"/",sep="")
+  newsitelist<-readRDS(paste(resloc,"newsite.RDS",sep=""))
+  
+  if(length(newsitelist)==1){
+    tempo<-newsitelist==df_included$site[i]
+    if(tempo==T){
+      resloc2<-paste("../../Results/for_BioTIME/Terrestrial_plotlevel/",df_included$site[i],"/",sep="")
+    }else{
+      resloc2<-paste("../../Results/for_BioTIME/Terrestrial_plotlevel/",df_included$site[i],"/",newsitelist,"/",sep="")
+    }
+    st<-readRDS(paste(resloc2,"summary_df.RDS",sep=""))
+    st$STUDY_ID<-df_included$site[i]
+    st$newsite<-newsitelist
+    summary_table<-rbind(summary_table,st)
+  }else{
+    for(j in 1:length(newsitelist)){
+      resloc2<-paste("../../Results/for_BioTIME/Terrestrial_plotlevel/",df_included$site[i],"/",newsitelist[j],"/",sep="")
+      st<-readRDS(paste(resloc2,"summary_df.RDS",sep=""))
+      st$STUDY_ID<-df_included$site[i]
+      st$newsite<-newsitelist[j]
+      summary_table<-rbind(summary_table,st)
+    }
+  }
+}
+# reorganize
+summary_table<-summary_table%>%select(STUDY_ID,newsite,nsp,nint,nind,npos,nL,nU,nneg,L,U)
+
+summary_table<-summary_table%>%mutate(f_nind=nind/nint,
+                                      f_npos=npos/nint,
+                                      f_nL=nL/nint,
+                                      f_nU=nU/nint,
+                                      f_nneg=nneg/nint)
+saveRDS(summary_table,"../../Results/for_BioTIME/Terrestrial_plotlevel/summary_table.RDS")
+
+df<-summary_table%>%select(STUDY_ID,newsite,nsp,f_nind,f_nL,f_nU,f_nneg)
+xxm<-readRDS("../../DATA/for_BioTIME/BioTIME_public_private_metadata.RDS")
+xxm<-xxm%>%select(STUDY_ID,TAXA)
+df<-inner_join(df,xxm,"STUDY_ID")
+df <-df[order(df$TAXA),]
+dat<-t(df)
+colnames(dat)<-dat[1,]
+dat<-dat[-1,]
+nsp<-dat[2,]
+dat<-dat[-c(1:2),]
+
+pdf("../../Results/for_BioTIME/Terrestrial_plotlevel/summary_plot.pdf",width=85,height=10)
+op<-par(mar=c(12,5,5,1))
+x<-barplot(dat,main = paste("Terrestrial dynamics: min ",data_pt_thrs," yrs",sep=""),
+           xlab = "",ylab="Freq. of pairwise interaction",ylim=c(0,1.4),
+           cex.lab=2,cex.main=2,names.arg = dat[5,],las=2,
+           col = c("yellow","red","skyblue","green"))
+text(x = x, y = 1, label = paste(colnames(dat),"(",nsp,")",sep=""), pos = 3, cex = 1, col = "purple")
+#text(x = x, y = 1, label = colnames(dat), pos = 1, cex = 1.5, col = "purple")
+legend("top",horiz=T,bty="n",cex=1.2,
+       c("Independent","Synchrony when rare", "Synchrony when abundant","compensatory","site(species)"),
+       fill = c("yellow","red","skyblue","green","purple"))
+par(op)
+dev.off()
+
+##########################################################################
+
+
+
+
+
+
+
+
+
+
+
 
 
 
