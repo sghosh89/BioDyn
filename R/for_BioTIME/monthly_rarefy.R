bc09df49b52bd4e385f53a1719334c9e667ad2bd
sghosh89 <ghoshshyamolina89@gmail.com>
2021-06-02 21:19:54 +0200

    updated rarefy code

--- R/for_BioTIME/monthly_rarefy.R
+++ R/for_BioTIME/monthly_rarefy.R
@@ -1,3 +1,6 @@
+# This function tries to make consistent (number of sampling months) sampling effort
+# with maximum possible overlap of sampling months from each year
+
 set.seed(seed=101)
 library(dplyr)
 library(purrr)
@@ -21,6 +24,83 @@ monthly_rarefy<-function(study,resamples=100,field){
   # min months sampled per year throughout the study period
   min_samp<-min(nsample$nm)
   
+  # this is a list of months sampled for each year, some months in a year maybe sampled for multiple days
+  monthlist<-study%>%group_by(YEAR)%>%
+    summarize(um = unique(MONTH),nm= n_distinct(MONTH), min_m=min(MONTH),max_m=max(MONTH))%>%ungroup()
+  
+  # now we want min_samp of months (a common set as far as possible) from each year 
+   mydf<-data.frame(YEAR=nsample$YEAR,MONTH=NA)
+   months_peryr <- vector(mode = "list", length = nrow(mydf))
+   for (i in 1:nrow(mydf)){
+     yr<-mydf$YEAR[i]
+     mydf$YEAR[i]<-yr
+     tempo<-monthlist%>%filter(YEAR==yr)
+     mydf$MONTH[i]<-list(sort(tempo$um))
+     months_peryr[[i]]<-sort(tempo$um)
+   }
+  
+   # get the common months sampled throughout the study years
+   common_months<-Reduce(intersect, months_peryr)
+   
+   mydfc<-vector(mode = "list", length = nrow(mydf)) 
+   names(mydfc)<-mydf$YEAR
+   
+   # first check, are there any common months throughout years? 
+   # at least one common month make this non-zero
+   if(length(common_months)==0){ # no common months
+     cat("========== Not a single common month found throughout the years =========")
+     # then do usual rarefaction
+     
+     # need to fill in as it is
+     mydfc<-months_peryr
+     
+   }
+   
+   if(length(common_months)!=0){ # with common months
+     cat("--------- months common from each year =",paste(common_months, collapse = ", ")," -------- \n")
+     
+     for(i in 1:length(months_peryr)){
+       
+       mym<-months_peryr[[i]]
+       
+       if(length(common_months)==min_samp){ # common months are min sampling months for a year
+         selected_cms<-common_months
+       }else if (length(common_months)>1 & length(common_months)<min_samp){ # logically, lengh(common_months)<min_samp, min_samp>1
+         fm<-head(common_months,1) # first month
+         em<-tail(common_months,1) # end month
+         cmr<-c(fm:em) # common months basic range
+         needm<- min_samp - length(cmr) # this number of months needed
+         
+         # our goal is to choose possible nearest 'needm' months of fm and em
+         nearfm<-sort(abs(mym-fm), index.return=T)$ix[-1] # indicies arranged near to far, fm itself excluded 
+         nearem<-sort(abs(mym-em), index.return=T)$ix[-1] # indicies arranged near to far, em itself excluded 
+         if(needm%%2==0){# needm is even number
+           needm<-needm/2
+         }else{
+           needm<-(needm+1)/2
+         }
+         
+         # now choose equal number of nearest neighbors from fm and em
+         # but it should be a range expansion: so leftside limit should be < fm
+         # right side limit should be > em
+         
+         fm_extra<-mym[nearfm]
+         fm_extra<-fm_extra[fm_extra<fm]
+         fm_extra<-fm_extra[1:needm]
+         
+         em_extra<-mym[nearem]
+         em_extra<-em_extra[em_extra>em]
+         em_extra<-em_extra[1:needm]
+         
+         selected_cms<-sort(c(cmr,fm_extra,em_extra))
+       }
+       
+       mydfc[[i]]<-selected_cms
+       
+     }
+     
+   }
+  
   #	define a function to calculate the sum(field) for use on the rarefied sample
   sum_field <- interp(~sum(as.numeric(var), na.rm=T),
                       var= as.name("Value"))
@@ -28,13 +108,20 @@ monthly_rarefy<-function(study,resamples=100,field){
   zero_NA_filter <- interp(~y > x & !is.na(y), 
                            .values = list(y = as.name("Value"), x = 0))
   
-  study <- study %>% unite(col=ObsEventID, YEAR, MONTH, sep="_", remove=FALSE)
+  # Now, we need to select only those months for each year according to 'mydfc' list
+  study_upd<-c()
+  for(i in 1:length(mydfc)){
+    tempo<-study%>% filter(YEAR==names(mydfc)[i] & MONTH%in%mydfc[[i]])
+    study_upd<-rbind(study_upd,tempo)
+  }
+  
+  study_upd <- study_upd %>% unite(col=ObsEventID, YEAR, MONTH, sep="_", remove=FALSE)
   
   ##	initialise 
   rarefied_metrics <- data.frame()
   
   #not needed
-  #nsamples <- ungroup(study) %>%
+  #nsamples <- ungroup(study_upd) %>%
   #  group_by(YEAR) %>%
     # remove 0's or NA's (this is to catch any places where 
     # abundance wasn't actually recorded 
@@ -43,7 +130,7 @@ monthly_rarefy<-function(study,resamples=100,field){
     # calculate how many months sampled per year 
   #  dplyr::summarise(nsamples = n_distinct(ObsEventID)) 
   
-  study_nest<-study%>%group_by(ObsEventID,YEAR,MONTH)%>%
+  study_nest<-study_upd%>%group_by(ObsEventID,YEAR,MONTH)%>%
                       filter_(.dots=zero_NA_filter) %>%
                       nest(nest_cols=c("Species", "Value"))%>% 
                   ungroup()
@@ -76,5 +163,8 @@ monthly_rarefy<-function(study,resamples=100,field){
   rarefied_metrics<-rarefied_metrics%>%group_by(Species,YEAR)%>%
     dplyr::summarise(Value=mean(Value))%>%ungroup()
 }
+
+
+
 
 
