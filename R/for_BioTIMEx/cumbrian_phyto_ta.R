a14c880a2c8ea225ab56c8385c0cfa87bf6d690c
sghosh89 <ghoshshyamolina89@gmail.com>
2021-06-18 09:42:56 +0200

    edits for splist

--- R/for_BioTIMEx/cumbrian_phyto_ta.R
+++ R/for_BioTIMEx/cumbrian_phyto_ta.R
@@ -29,3 +29,27 @@ for(i in 1:length(sites)){
   tail_analysis(mat=spmat,resloc=myresloc,nbin=2)
 }
 
+#=================================================================
+# make species list 
+sp1<-readRDS("../../DATA/for_BioTIMEx/wrangled_data/cumbrian_phyto/cumbrian_phyto_BLEL_inputmatrix_tailanal.RDS")
+sp1<-colnames(sp1)
+sp2<-readRDS("../../DATA/for_BioTIMEx/wrangled_data/cumbrian_phyto/cumbrian_phyto_ESTH_inputmatrix_tailanal.RDS")
+sp2<-colnames(sp2)
+sp3<-readRDS("../../DATA/for_BioTIMEx/wrangled_data/cumbrian_phyto/cumbrian_phyto_NBAS_inputmatrix_tailanal.RDS")
+sp3<-colnames(sp3)
+sp4<-readRDS("../../DATA/for_BioTIMEx/wrangled_data/cumbrian_phyto/cumbrian_phyto_SBAS_inputmatrix_tailanal.RDS")
+sp4<-colnames(sp4)
+
+splist<-c(sp1,sp2,sp3,sp4)
+splist<-sort(unique(splist))
+splist<-data.frame(code=splist,sp=NA)
+write.csv(splist,"../../DATA/for_BioTIMEx/wrangled_data/cumbrian_phyto/splist.csv",row.names = F)
+# now manually fill in the 2nd column and saved as splist_edited.csv
+
+
+
+
+
+
+
+
