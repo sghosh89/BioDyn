26c54cb756ae8070220990dd4003cddcf58aaab6
sghosh89 <ghoshshyamolina89@gmail.com>
2021-06-24 19:07:17 +0200

    edits

--- R/for_insectRoel/Master_insect.R
+++ R/for_insectRoel/Master_insect.R
@@ -2,3 +2,5 @@ path<-dirname(rstudioapi::getSourceEditorContext()$path)
 setwd(path)
 source("wrangling_data.R") # clean and prepare data
 source("insect_ta.R") # tail analysis
+source("rank_category.R") 
+source("summary_for_stabilitymetric.R")
