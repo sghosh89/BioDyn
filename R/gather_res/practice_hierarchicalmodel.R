8a5653a77b0d6321c248ac423af0ff13320634bd
sghosh89 <ghoshshyamolina89@gmail.com>
2021-06-28 22:07:50 +0200

    whole data modeling

--- R/gather_res/practice_hierarchicalmodel.R
+++ R/gather_res/practice_hierarchicalmodel.R
@@ -3,19 +3,19 @@ library(brms)
 library(tidyverse)
 
 #========================================================================================
-sink("./console_practice_hierarchicalmodel_skwnormalfamily.txt", append=TRUE, split=TRUE)
+sink("./console_practice_hierarchicalmodel.txt", append=TRUE, split=TRUE)
 #====================================================================================================
+
 
 #===========================
 df<-readRDS("../../Results/gather_res/stability_metric_all.RDS")
-
 mydat<-df[,c("source","STUDY_ID","newsite","REALM","TAXA","ORGANISMS","iCV","iCValt","phi","phi_skw","nsp","L","U","f_nL","f_nU")]
+mydat$UID<-paste(mydat$source,mydat$STUDY_ID,sep=",")
 mydat$A<-mydat$f_nL+mydat$f_nU # total asymmetry
 mydat$uniA<-mydat$f_nL-mydat$f_nU # net asymmetry
 mydat$community<-"NT" #Symmetric
 mydat$community[which(mydat$L>abs(mydat$U))]<-"LT"
 mydat$community[which(mydat$L<abs(mydat$U))]<-"UT"
-#mydat<-mydat%>%filter(community%in%c("LT","UT"))
 
 mydat<-mydat%>%rename(
   stability=iCV,
@@ -23,7 +23,8 @@ mydat<-mydat%>%rename(
   VR=phi,
   SR=phi_skw,
   R=nsp)
-mydat<-na.omit(mydat) # one community G8554 has dominant target sp. varying indep with each other, so 
+mydat<-na.omit(mydat) # one RivFishTIME data point omitted as SR=NaN
+# one community G8554 has dominant target sp. varying indep with each other, so 
 # SR was not calculated - VR showing compensatory dynamics (<1) due to other common sp not considered
 # in tail-analysis.
 #============================================================================
@@ -49,10 +50,10 @@ mydat_scaled$uniA<- scale(mydat_scaled$uniA)
 
 # 237 newsites are arranged in the following hierarchical structure
 #xx<-mydat_scaled%>%group_by(REALM,STUDY_ID,Protocol)%>%count()%>%ungroup()
-xx<-mydat_scaled%>%group_by(REALM,TAXA,STUDY_ID)%>%count()%>%ungroup()
+xx<-mydat_scaled%>%group_by(REALM,TAXA,UID)%>%count()%>%ungroup()
 sum(xx$n)==nrow(mydat)
 
-ggplot(data=mydat_scaled,aes(y=stability_skw,x=R,col=STUDY_ID,shape=REALM))+
+ggplot(data=mydat_scaled,aes(y=stability_skw,x=R,col=UID,shape=REALM))+
   geom_point(size=1.2,alpha=0.8,position="jitter")+
   geom_smooth(method = lm,
               se     = FALSE,
@@ -69,37 +70,44 @@ cat(paste("------- brms null model starting at time: ", Sys.time()," -----------
 
 # write the formula  
 bf_stability0<-bf(stability_skw ~ (R+VR)*REALM+
-                    (1+ (R+VR)*REALM |TAXA/STUDY_ID)
-)
+                    (1+ (R+VR)*REALM |TAXA/UID))
 
 null_model<-brm(bf_stability0,
                 data=mydat_scaled,
-                family = skew_normal(),
-                chains=2,cores=2,iter=10000,
-                control = list(adapt_delta = 0.99, max_treedepth = 20),
+                #family = skew_normal(),
+                chains=4,cores=4,iter=10000,
+                control = list(adapt_delta = 0.99, max_treedepth = 15),
                 save_pars = save_pars(all = TRUE),seed=123)
 print(summary(null_model),digits = 3)
-null_model<-add_criterion(null_model,
+saveRDS(null_model,"../../Results/gather_res/pratice_hierarchical_nullmodel.RDS")
+
+null_model_c<-add_criterion(null_model,
                           criterion=c("loo", "waic", "loo_R2", "bayes_R2"),
                           moment_match = T,
                           reloo = T)
-saveRDS(null_model,"../../Results/gather_res/pratice_hierarchical_nullmodel_skwnormal.RDS")
+saveRDS(null_model_c,"../../Results/gather_res/pratice_hierarchical_nullmodel_withcrit.RDS")
 
 
 cat(paste("------- brms full model starting at time: ", Sys.time()," -------------- \n "))
 
 bf_stability<-bf(stability_skw ~ (R+VR+A+uniA+SR)*REALM+ 
-                   (1+(R+VR+A+uniA+SR)*REALM|TAXA/STUDY_ID))
+                   (1+(R+VR+A+uniA+SR)*REALM|TAXA/UID))
 
-full_model<-update(null_model, bf_stability, 
-                   newdata = mydat_scaled)
+full_model<-brm(bf_stability,
+                data=mydat_scaled,
+                #family = skew_normal(),
+                chains=4,cores=4,iter=10000,
+                control = list(adapt_delta = 0.99, max_treedepth = 15),
+                save_pars = save_pars(all = TRUE),seed=123)
 
 print(summary(full_model),digits = 3)
-full_model<-add_criterion(full_model,
+saveRDS(full_model,"../../Results/gather_res/pratice_hierarchical_fullmodel.RDS")
+
+full_model_c<-add_criterion(full_model,
                           criterion=c("loo", "waic", "loo_R2", "bayes_R2"),
                           moment_match = T,
                           reloo = T)
-saveRDS(full_model,"../../Results/gather_res/pratice_hierarchical_fullmodel_skwnormal.RDS")
+saveRDS(full_model_c,"../../Results/gather_res/pratice_hierarchical_fullmodel_withcrit.RDS")
 
 cat("==== compare loo for both model =====","\n")
 loo_null<-loo(null_model)
@@ -107,17 +115,17 @@ loo_full<-loo(full_model)
 loo_compare(loo_null,loo_full)
 loo_model_weights(null_model,full_model)
 
-cat("==== R2 for null model: loo =====","\n")
-loo_R2(null_model)
+#cat("==== R2 for null model: loo =====","\n")
+#loo_R2(null_model)
 
-cat("==== R2 for full model: loo =====","\n")
-loo_R2(full_model)
+#cat("==== R2 for full model: loo =====","\n")
+#loo_R2(full_model)
 
-cat("==== R2 for null model: bayes =====","\n")
-bayes_R2(null_model)
+#cat("==== R2 for null model: bayes =====","\n")
+#bayes_R2(null_model)
 
-cat("==== R2 for full model: bayes =====","\n")
-bayes_R2(full_model)
+#cat("==== R2 for full model: bayes =====","\n")
+#bayes_R2(full_model)
 
 # to get marginal (variance explained by fixed effects only) R2
 #    and conditional (variance explained by fixed+random effects) R2 
@@ -133,22 +141,22 @@ sink()
 #################################
 
 # to see estimates from random effects
-xr<-ranef(null_model)
-xr1<-xr$STUDY_ID
-names(xr1)
-xr11<-xr1[,,1]
-xr12<-xr1[,,2]
+#xr<-ranef(null_model)
+#xr1<-xr$STUDY_ID
+#names(xr1)
+#xr11<-xr1[,,1]
+#xr12<-xr1[,,2]
 # is it a reasonable fit?
-#pp_check(null_model,nsamples = 10)
+#pp_check(null_model,nsamples = 100)
 #pp_check(full_model,nsamples = 10)
 #conditional_effects(null_model)
 #conditional_effects(full_model)
-null_model<-readRDS("../../Results/gather_res/pratice_hierarchical_nullmodel_skwnormal.RDS")
-full_model<-readRDS("../../Results/gather_res/pratice_hierarchical_fullmodel_skwnormal.RDS")
+#null_model<-readRDS("../../Results/gather_res/pratice_hierarchical_nullmodel_skwnormal.RDS")
+#full_model<-readRDS("../../Results/gather_res/pratice_hierarchical_fullmodel_skwnormal.RDS")
 
-brms::pp_check(null_model)
-brms::pp_check(full_model)
-conditional_effects(full_model)
+#brms::pp_check(null_model,nsamples = 100)
+#brms::pp_check(full_model,nsamples = 100)
+#brms::conditional_effects(full_model)
 
 
 
