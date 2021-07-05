d56f0d024b1641223ef2daf5e4c2e603fc36055d
sghosh89 <ghoshshyamolina89@gmail.com>
2021-07-06 01:23:34 +0200

    stability metric computed only for dominant sp

--- R/for_RivFishTIME/wrangling_data.R
+++ R/for_RivFishTIME/wrangling_data.R
@@ -4,6 +4,8 @@ library(tidyverse)
 # read the data
 x<-read.csv("../../DATA/for_RivFishTIME/raw_data/RivFishTIME_accessed_08dec2020/1873_2_RivFishTIME_SurveyTable.csv") # a dataframe
 str(x)
+unique(x$Quarter)
+x$Quarter<-trimws(x$Quarter) # remove white space
 x_meta<-read.csv("../../DATA/for_RivFishTIME/raw_data/RivFishTIME_accessed_08dec2020/1873_2_RivFishTIME_TimeseriesTable.csv")
 #x_readme<-read.csv("../../DATA/for_RivFishTIME/raw_data/RivFishTIME_accessed_08dec2020/1873_2_Readme.csv")
 z<-x %>% distinct(TimeSeriesID, .keep_all = TRUE)%>%select(TimeSeriesID,UnitAbundance)
@@ -17,34 +19,37 @@ c<-x%>%group_by(TimeSeriesID)%>%summarise(nyr=n_distinct(Year),
                                           nsurvey=n_distinct(SurveyID),
                                           nunit=n_distinct(UnitAbundance))%>%ungroup()
 unique(c$nunit) # 1: so it ensures we always had same unit of abundance for a given TimeSeriesID 
-
-# but we will consider only those having >=20 years data
-c20<-c%>%filter(nyr>=20) # there are 967 timeseries like that
-
-table(c20$uq) # this table shows quarter 3,4 are the most frequent sampling time
+table(c$uq) # this table shows quarter 3,4 are the most frequent sampling time
 
 # so to make consistent sampling effort, 
-# we will consider sites which were sampled for both quarter 3, 4 (July-Dec)
-c20_q3q4<-c20%>%filter(uq%in%c("3","4"))
-table(c20_q3q4$uq) 
-c20_q3q4<-c20_q3q4%>%group_by(TimeSeriesID)%>%
-                    summarise(q3q4=n_distinct(uq))%>%ungroup()%>%
-                    filter(q3q4==2) 
+# we will consider sites which were sampled for both quarter 3, 4 (July-Dec) and atleast for 20 years
+x_q3q4<-x%>%filter(Quarter%in%c("3","4"))
+x20_q3q4<-x_q3q4%>%group_by(TimeSeriesID)%>%
+                    summarise(nyr=n_distinct(Year),
+                              q3q4=n_distinct(Quarter))%>%ungroup()%>%
+                    filter(nyr>=20 & q3q4==2)
 
-c20_q3q4<-inner_join(c20_q3q4,x_meta,by="TimeSeriesID")%>%select(-c(q3q4,X))
 
-# so, there are 829 sites which were sampled in both quarters: q3, q4 (July-Dec)
-write.csv(c20_q3q4,"../../DATA/for_RivFishTIME/wrangled_data/TimeSeriesID_sampled_bothq3q4.csv",row.names = F)
+x20_q3q4<-inner_join(x20_q3q4,x_meta,by="TimeSeriesID")%>%select(-c(q3q4,X))
+
+# so, there are 693 sites which were sampled in both quarters: q3, q4 (July-Dec)
+write.csv(x20_q3q4,"../../DATA/for_RivFishTIME/wrangled_data/TimeSeriesID_sampled_bothq3q4.csv",row.names = F)
 
 ################################################################################################################
 # Now filtering main data for abundance (unique(x$UnitAbundance)="Ind.100m2", "CPUE", "Count" )
+xq3q4<-x%>%filter(TimeSeriesID%in%x20_q3q4$TimeSeriesID & Quarter%in%c("3","4"))
+table(xq3q4$UnitAbundance)
+#Count      CPUE Ind.100m2 
+#115809      2677     40972
+# we will only consider the "Count" - it is the majority in data? then only 180 TimeSeriesID will be there
+#xq3q4<-xq3q4%>%filter(UnitAbundance=="Count")
+xtbl<-xq3q4%>%group_by(TimeSeriesID)%>%summarize(n=n_distinct(UnitAbundance))%>%ungroup()
+all(xtbl$n==1)# so, each timeseriesID has unique unit
 
-xq3q4<-x%>%filter(TimeSeriesID%in%c20_q3q4$TimeSeriesID & Quarter%in%c("3","4"))%>%
+xq3q4<-xq3q4%>%
         group_by(TimeSeriesID,Year,Species)%>%
         summarise(Abundance=sum(Abundance))%>%ungroup()
         
-all(unique(xq3q4$TimeSeriesID)==unique(c20_q3q4$TimeSeriesID))==T
-
 ts_ids<-unique(xq3q4$TimeSeriesID)
 
 bad_TimeSeriesID_w_singlesp<-c()
@@ -84,7 +89,7 @@ for(i in 1:length(ts_ids)){
   
   if(minsp>=2){ # atleast 2 species should be present at a site to consider
     saveRDS(commonsp_mat,paste(tsid_resloc,"commonspecies_timeseries.RDS",sep=""))
-    metafile<-c20_q3q4%>%filter(TimeSeriesID==ts_id)
+    metafile<-x20_q3q4%>%filter(TimeSeriesID==ts_id)
     saveRDS(metafile,paste(tsid_resloc,"metadata.RDS",sep=""))
   }else{
     bad_TimeSeriesID_w_singlesp<-c(bad_TimeSeriesID_w_singlesp,ts_id)
