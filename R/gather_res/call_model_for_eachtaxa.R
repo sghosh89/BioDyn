eae06fc00cebf3ea96ae5037cd69205da3473400
sghosh89 <ghoshshyamolina89@gmail.com>
2021-07-08 21:53:10 +0200

    taxawise run

--- R/gather_res/call_model_for_eachtaxa.R
+++ R/gather_res/call_model_for_eachtaxa.R
@@ -1,8 +1,9 @@
 library(brms)
+library(performance)
 # mydat: full data
 # taxa: specific taxa you want to select
 
-call_model_for_eachtaxa<-function(mydat, taxa){
+call_model_for_eachtaxa<-function(mydat, taxa, resloc){
   
   mydat_raw<-mydat%>%filter(TAXA==taxa)
   mydat_scaled<-mydat_raw
@@ -14,44 +15,73 @@ call_model_for_eachtaxa<-function(mydat, taxa){
   mydat_scaled$A<- scale(mydat_scaled$A)
   mydat_scaled$uniA<- scale(mydat_scaled$uniA)
   
-  myresloc<-paste("../../Results/gather_res/modelres_",taxa,"/",sep="")
+  myresloc<-paste(resloc,"/modelres_",taxa,"/",sep="")
   
   if(!dir.exists(myresloc)){
     dir.create(myresloc)
   }
   
   usedat<-list(mydat_raw=mydat_raw,mydat_scaled=mydat_scaled)
-  saveRDS(usedat,paste("../../Results/gather_res/modelres_",taxa,"/usedat.RDS",sep=""))
+  saveRDS(usedat,paste(myresloc,"usedat.RDS",sep=""))
   
   cat(paste("------- brms null model starting for ", taxa, " at time: ", Sys.time()," -------------- \n "))
   
-  bf_stability0<-bf(stability_skw ~ (R+VR)+ (1+ (R+VR)|UID))
+  bf_stability0<-bf(stability_skw ~ (R+VR)+ (1|UID))
   
   null_model<-brm(bf_stability0,
                   data=mydat_scaled,
-                  chains=4,cores=4,iter=10000,
+                  family = gaussian(),
+                  chains=4,cores=4,iter=16000,
+                  warmup=8000,init="0",thin=4,
                   control = list(adapt_delta = 0.99, max_treedepth = 15),
                   save_pars = save_pars(all = TRUE),seed=123)
   
-  saveRDS(null_model,paste("../../Results/gather_res/modelres_",taxa,"/nullmodel.RDS",sep=""))
+  saveRDS(null_model,paste(myresloc,"nullmodel.RDS",sep=""))
 
   print(summary(null_model),digits = 3)
   
-  cat(paste("------- brms full model starting at time: ", Sys.time()," -------------- \n "))
+  cat(paste("------- brms full model starting for ", taxa, " at time: ", Sys.time()," -------------- \n "))
   
-  bf_stability<-bf(stability_skw ~ (R+VR+A+uniA+SR)+(1+(R+VR+A+uniA+SR)|UID))
+  bf_stability<-bf(stability_skw ~ (R+VR+A+uniA+SR)+(1|UID))
   
-  #full_model<-update(null_model, bf_stability, 
-  #                   newdata = mydat_scaled)
   full_model<-brm(bf_stability,
-                  data=mydat_scaled,
-                  chains=4,cores=4,iter=10000,
-                  control = list(adapt_delta = 0.99, max_treedepth = 15),
-                  save_pars = save_pars(all = TRUE),seed=123)
+                    data=mydat_scaled,
+                    family = gaussian(),
+                    chains=4,cores=4,iter=16000,
+                    warmup=8000,init="0",thin=4,
+                    control = list(adapt_delta = 0.99, max_treedepth = 15),
+                    save_pars = save_pars(all = TRUE),seed=123)
+  
   
   print(summary(full_model),digits = 3)
   
-  saveRDS(full_model,paste("../../Results/gather_res/modelres_",taxa,"/fullmodel.RDS",sep=""))
+  saveRDS(full_model,paste(myresloc,"fullmodel.RDS",sep=""))
+  
+  cat(paste("------- brms compare model starting for ", taxa, " at time: ", Sys.time()," -------------- \n "))
+  
+  loo_null<-loo(null_model,moment_match=T,reloo=T)
+  loo_full<-loo(full_model,moment_match=T,reloo=T)
+  lc<-loo_compare(loo_null,loo_full)
+  lmw<-loo_model_weights(null_model,full_model)
+  lc_list<-list(loo_null=loo_null,
+                loo_full=loo_full,
+                lc=lc,
+                lmw=lmw)
+  
+  saveRDS(lc_list,paste(myresloc,"lc_list.RDS",sep=""))
+  
+  cat(paste("------- brms R2 starting for ", taxa, " at time: ", Sys.time()," -------------- \n "))
+  
+  cat("==== R2 for null model: bayes =====","\n")
+  r2_null<-r2_bayes(null_model,ci=0.95)
+  r2_null
+  cat("==== R2 for full model: bayes =====","\n")
+  r2_full<-r2_bayes(full_model,ci=0.95)
+  r2_full
+  
+  r2_list<-list(r2_null=r2_null,
+                r2_full=r2_full)
+  saveRDS(r2_list,paste(myresloc,"r2_list.RDS",sep=""))
   
 }
 
--- R/gather_res/practice_hierarchicalmodel_taxawise.R
+++ R/gather_res/practice_hierarchicalmodel_taxawise.R
@@ -2,9 +2,15 @@ rm(list=ls())
 source("call_model_for_eachtaxa.R")
 library(brms)
 library(tidyverse)
+library(performance)
+
+if(!dir.exists("../../Results/gather_res/test_eachtaxa")){
+  dir.create("../../Results/gather_res/test_eachtaxa")
+}
+
 
 #========================================================================================
-sink("./console_practice_hierarchicalmodel_taxawise.txt", append=TRUE, split=TRUE)
+sink("../../Results/gather_res/test_eachtaxa/console_practice_hierarchicalmodel_taxawise.txt", append=TRUE, split=TRUE)
 #====================================================================================================
 
 #===========================
@@ -13,9 +19,6 @@ mydat<-df[,c("source","STUDY_ID","newsite","REALM","TAXA","ORGANISMS","iCV","iCV
 mydat$UID<-paste(mydat$source,mydat$STUDY_ID,sep=",")
 mydat$A<-mydat$f_nL+mydat$f_nU # total asymmetry
 mydat$uniA<-mydat$f_nL-mydat$f_nU # net asymmetry
-mydat$community<-"NT" #Symmetric
-mydat$community[which(mydat$L>abs(mydat$U))]<-"LT"
-mydat$community[which(mydat$L<abs(mydat$U))]<-"UT"
 
 mydat<-mydat%>%rename(
   stability=iCV,
@@ -23,11 +26,12 @@ mydat<-mydat%>%rename(
   VR=phi,
   SR=phi_skw,
   R=nsp)
-mydat<-na.omit(mydat) # one RivFishTIME data point omitted as SR=NaN
-
 #============================================================================
 table(mydat$TAXA) # mammals for terrestrial, terrestrial data are 
 # always large in number than freshwater
+class(mydat_scaled$REALM)
+mydat$TAXA<-as.factor(mydat$TAXA)
+mydat$UID<-as.factor(mydat$UID)
 #============================================================================
 # freshwater & terrestrial
 call_model_for_eachtaxa(mydat=mydat,taxa="fish")
