e188f15477ad43f75fb67cd03872557a2fd31bdb
sghosh89 <ghoshshyamolina89@gmail.com>
2021-07-12 15:18:21 +0200

    wo scaling run

--- R/gather_res/practice_hierarchicalmodel_test_wo_scaling.R
+++ R/gather_res/practice_hierarchicalmodel_test_wo_scaling.R
@@ -7,9 +7,12 @@ if(!dir.exists("../../Results/gather_res/test")){
   dir.create("../../Results/gather_res/test")
 }
 
+if(!dir.exists("../../Results/gather_res/test/wo_scaling")){
+  dir.create("../../Results/gather_res/test/wo_scaling")
+}
 
 #========================================================================================
-sink("../../Results/gather_res/test/console_practice_hierarchicalmodel_test.txt", append=TRUE, split=TRUE)
+sink("../../Results/gather_res/test/wo_scaling/console_practice_hierarchicalmodel_test_wo_scaling.txt", append=TRUE, split=TRUE)
 #====================================================================================================
 
 
@@ -32,59 +35,9 @@ mydat<-mydat%>%rename(
 #============================================================================
 table(mydat$TAXA) # mammals for terrestrial, terrestrial data are 
                   # always large in number than freshwater
-
 mydat$TAXA<-as.factor(mydat$TAXA)
 mydat$UID<-as.factor(mydat$UID)
 #============================================================================
-# first, we should scale the variables we are going to consider
-# scaled quantity = (actual quantity -mean(actual quantity))/sd(actual quantity)
-mydat_scaled<-mydat
-mydat_scaled$stability<-scale(mydat_scaled$stability)
-mydat_scaled$stability_skw<-scale(mydat_scaled$stability_skw)
-mydat_scaled$R<-scale(mydat_scaled$R)
-mydat_scaled$VR<- scale(mydat_scaled$VR) 
-mydat_scaled$SR<- scale(mydat_scaled$SR)
-mydat_scaled$A<- scale(mydat_scaled$A)
-mydat_scaled$uniA<- scale(mydat_scaled$uniA)
-
-class(mydat_scaled$REALM)
-#=====================================================
-
-# (R+VR)*REALM = R + VR + REALM + R:REALM + VR:REALM
-
-cat("------- Hierarchical structure in the data -------------- \n ")
-xx<-mydat_scaled%>%group_by(REALM,TAXA,UID)%>%count()%>%ungroup()
-#sum(xx$n)==nrow(mydat)
-
-ggplot(data=mydat_scaled,aes(y=stability_skw,x=R,col=TAXA,shape=REALM))+
-  geom_point(size=1.2,alpha=0.8,position="jitter")+
-  geom_smooth(method = lm,
-              se     = FALSE,
-              size   = .5, 
-              alpha  = .8)+
-  theme(legend.position = "bottom")
-
-dt<-mydat_scaled%>%filter(TAXA=="fish")
-ggplot(data=dt,aes(y=stability_skw,x=R,col=UID))+
-  geom_point(size=1.2,alpha=0.8,position="jitter")+
-  geom_smooth(method = lm,
-              se     = FALSE,
-              size   = .5, 
-              alpha  = .8)+
-  theme(legend.position = "none")
-
-ggplot(data=dt,aes(y=stability_skw,x=VR,col=UID))+
-  geom_point(size=1.2,alpha=0.8,position="jitter")+
-  geom_smooth(method = lm,
-              se     = FALSE,
-              size   = .5, 
-              alpha  = .8)+
-  theme(legend.position = "none")
-
-
-#mydat_scaled<-mydat_scaled%>%filter(TAXA%in%c("birds","fish",
-#                                              "freshwater invertebrates","terrestrial invertebrates",
-#                                              "freshwater plants", "terrestrial plants"))
 
 
 #(1 + REALM |STUDY_ID) = The effect of REALM will vary between STUDY_ID.
@@ -99,33 +52,33 @@ cat(paste("------- brms null model without REALM starting at time: ", Sys.time()
 
 bf_stability0_wo_REALM<-bf(stability_skw ~ (R+VR) + (1|TAXA/UID))
 null_model_wo_REALM<-brm(bf_stability0_wo_REALM,
-                data=mydat_scaled,
+                data=mydat,
                 family = gaussian(),
                 chains=4,cores=4,iter=16000,
                 warmup=8000,init="0",thin=4,
                 control = list(adapt_delta = 0.99, max_treedepth = 15),
                 save_pars = save_pars(all = TRUE),seed=123)
 print(summary(null_model_wo_REALM),digits = 3)
-saveRDS(null_model_wo_REALM,"../../Results/gather_res/test/nullmodel_wo_REALM.RDS")
+saveRDS(null_model_wo_REALM,"../../Results/gather_res/test/wo_scaling/nullmodel_wo_REALM.RDS")
 
 cat(paste("------- brms null model with REALM starting at time: ", Sys.time()," -------------- \n "))
 
 bf_stability0_w_REALM<-bf(stability_skw ~ (R+VR)*REALM + (1|TAXA/UID))
 null_model_w_REALM<-brm(bf_stability0_w_REALM,
-                         data=mydat_scaled,
+                         data=mydat,
                          family = gaussian(),
                          chains=4,cores=4,iter=16000,
                          warmup=8000,init="0",thin=4,
                          control = list(adapt_delta = 0.99, max_treedepth = 15),
                          save_pars = save_pars(all = TRUE),seed=123)
 print(summary(null_model_w_REALM),digits = 3)
-saveRDS(null_model_w_REALM,"../../Results/gather_res/test/nullmodel_w_REALM.RDS")
+saveRDS(null_model_w_REALM,"../../Results/gather_res/test/wo_scaling/nullmodel_w_REALM.RDS")
 
 cat(paste("------- brms full model starting at time: ", Sys.time()," -------------- \n "))
 bf_stability<-bf(stability_skw ~ (R+VR+A+uniA+SR)*REALM + (1|TAXA/UID))
 
 full_model<-brm(bf_stability,
-                data=mydat_scaled,
+                data=mydat,
                 family = gaussian(),
                 chains=4,cores=4,iter=16000,
                 warmup=8000,init="0",thin=4,
@@ -133,14 +86,7 @@ full_model<-brm(bf_stability,
                 save_pars = save_pars(all = TRUE),seed=123)
 
 print(summary(full_model),digits = 3)
-saveRDS(full_model,"../../Results/gather_res/test/fullmodel.RDS")
-
-
-#null_model_c<-add_criterion(null_model,
-#                            criterion=c("loo", "loo_R2"),
-#                            moment_match = T,
-#                            reloo = T)
-#saveRDS(null_model_c,"../../Results/gather_res/pratice_hierarchical_nullmodel_withcrit.RDS")
+saveRDS(full_model,"../../Results/gather_res/test/wo_scaling/fullmodel.RDS")
 
 
 cat(paste("------- compare loo for all models: ", Sys.time()," -------------- \n "))
@@ -154,7 +100,7 @@ lc_list<-list(loo_null_wo_REALM=loo_null_wo_REALM,
               loo_full=loo_full,
               lc=lc,
               lmw=lmw)
-saveRDS(lc_list,"../../Results/gather_res/test/lc_list.RDS")
+saveRDS(lc_list,"../../Results/gather_res/test/wo_scaling/lc_list.RDS")
 print(lc)
 print(lmw)
 
@@ -177,50 +123,10 @@ r2_full
 r2_list<-list(r2_null_wo_REALM=r2_null_wo_REALM,
               r2_null_w_REALM=r2_null_w_REALM,
               r2_full=r2_full)
-saveRDS(r2_list,"../../Results/gather_res/test/r2_list.RDS")
+saveRDS(r2_list,"../../Results/gather_res/test/wo_scaling/r2_list.RDS")
 
 cat(paste("------- completed: ", Sys.time()," -------------- \n "))
 
 sink()
 
 #################################
-null_model_wo_REALM<-readRDS("../../Results/gather_res/test/nullmodel_wo_REALM.RDS")
-null_model_w_REALM<-readRDS("../../Results/gather_res/test/nullmodel_w_REALM.RDS")
-full_model<-readRDS("../../Results/gather_res/test/fullmodel.RDS")
-
-# is it a reasonable fit?
-#brms::pp_check(null_model_wo_REALM,nsamples = 1000)
-#brms::pp_check(null_model_w_REALM,nsamples = 1000)
-#brms::pp_check(full_model,nsamples = 1000)
-
-conditional_effects(null_model_wo_REALM)
-conditional_effects(null_model_w_REALM)
-conditional_effects(full_model)
-
-# to see estimates
-fixef(full_model,probs = c(0.025, 0.125, 0.875, 0.975))
-ranobj<-ranef(full_model)
-obj<-coef(full_model) # this is the combined effect of fixed and random effect
-  
-# to see estimates from random effects
-names(ranobj)
-ranobj$TAXA
-ranobj$`TAXA:UID`
-
-
-# test case
-objf<-fixef(null_model_w_REALM)
-objr<-ranef(null_model_w_REALM)
-objr$TAXA
-objc<-coef(null_model_w_REALM)
-objc$TAXA[,,"Intercept"]
-
-# fixed effect + random effect == coef effect
-objf["Intercept","Estimate"]+objr$TAXA[,,"Intercept"][1,1]==objc$TAXA[,,"Intercept"][1,1]
-
-## question: what are r_ terms in posterior_samples(full_model)?
-
-
-
-
-
