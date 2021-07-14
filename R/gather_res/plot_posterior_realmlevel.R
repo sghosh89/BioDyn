4e5d1116cb77896b11cbf93fdfd44a6acb8352ae
sghosh89 <ghoshshyamolina89@gmail.com>
2021-07-14 13:57:06 +0200

    edits

--- R/gather_res/plot_posterior_realmlevel.R
+++ R/gather_res/plot_posterior_realmlevel.R
@@ -26,7 +26,8 @@ gathered_post$color[which(gathered_post$key==keys[1])]<-3
 gathered_post$color[which(gathered_post$key==keys[2])]<-2
 gathered_post$color[which(gathered_post$key==keys[3])]<-1
 
-g_I<-gathered_post %>% 
+gathered_postI<-gathered_post
+g_I<-gathered_postI %>% 
   ggplot(aes(x = value, group = key, fill=as.factor(color))) +
   geom_vline(xintercept = 0, linetype="dashed", color="dimgray")+
   geom_density(size = .2, alpha=0.6)+guides(fill=FALSE, color=FALSE)+
@@ -58,7 +59,8 @@ gathered_post <-
   gathered_post$color[which(gathered_post$key==keys[2])]<-2
   gathered_post$color[which(gathered_post$key==keys[3])]<-1
 
-g_R<-gathered_post %>% 
+gathered_postR<-gathered_post
+g_R<-gathered_postR %>% 
   ggplot(aes(x = value, group = key, fill=as.factor(color))) +
   geom_vline(xintercept = 0, linetype="dashed", color="dimgray")+
   geom_density(size = .2, alpha=0.6)+guides(fill=FALSE, color=FALSE)+
@@ -94,7 +96,8 @@ gathered_post$color[which(gathered_post$key==keys[1])]<-3
 gathered_post$color[which(gathered_post$key==keys[2])]<-2
 gathered_post$color[which(gathered_post$key==keys[3])]<-1
 
-g_VR<-gathered_post %>% 
+gathered_postVR<-gathered_post
+g_VR<-gathered_postVR %>% 
   ggplot(aes(x = value, group = key, fill=as.factor(color))) +
   geom_vline(xintercept = 0, linetype="dashed", color="dimgray")+
   geom_density(size = .2, alpha=0.6)+guides(fill=FALSE, color=FALSE)+
@@ -126,7 +129,8 @@ gathered_post$color[which(gathered_post$key==keys[1])]<-3
 gathered_post$color[which(gathered_post$key==keys[2])]<-2
 gathered_post$color[which(gathered_post$key==keys[3])]<-1
 
-g_A<-gathered_post %>% 
+gathered_postA<-gathered_post
+g_A<-gathered_postA %>% 
   ggplot(aes(x = value, group = key, fill=as.factor(color))) +
   geom_vline(xintercept = 0, linetype="dashed", color="dimgray")+
   geom_density(size = .2, alpha=0.6)+guides(fill=FALSE, color=FALSE)+
@@ -158,7 +162,8 @@ gathered_post$color[which(gathered_post$key==keys[1])]<-3
 gathered_post$color[which(gathered_post$key==keys[2])]<-2
 gathered_post$color[which(gathered_post$key==keys[3])]<-1
 
-g_uniA<-gathered_post %>% 
+gathered_postuniA<-gathered_post
+g_uniA<-gathered_postuniA %>% 
   ggplot(aes(x = value, group = key, fill=as.factor(color))) +
   geom_vline(xintercept = 0, linetype="dashed", color="dimgray")+
   geom_density(size = .2, alpha=0.6)+guides(fill=FALSE, color=FALSE)+
@@ -191,7 +196,8 @@ gathered_post$color[which(gathered_post$key==keys[1])]<-3
 gathered_post$color[which(gathered_post$key==keys[2])]<-2
 gathered_post$color[which(gathered_post$key==keys[3])]<-1
 
-g_SR<-gathered_post %>% 
+gathered_postSR<-gathered_post
+g_SR<-gathered_postSR %>% 
   ggplot(aes(x = value, group = key, fill=as.factor(color))) +
   geom_vline(xintercept = 0, linetype="dashed", color="dimgray")+
   geom_density(size = .2, alpha=0.6)+guides(fill=FALSE, color=FALSE)+
@@ -208,3 +214,199 @@ op<-par(mar=c(2,2,2,2))
 gridExtra::grid.arrange(g_I,g_R,g_VR,g_A,g_uniA,g_SR,nrow=6)
 par(op)
 dev.off()
+
+#=================================================================================
+#  point estimate plot
+#=================================================================================
+g_I<-gathered_postI%>%ggplot(aes(y=key,x=value,col=as.factor(color)))+
+  geom_vline(xintercept = 0, linetype="dashed", color="dimgray")+
+  guides(fill=FALSE, color=FALSE)+
+  stat_pointinterval(.width=c(0.75,0.95))+
+  annotate("text", x=-1, y=3.4, label= "Intercept", size=5)+
+  theme_bw(base_size = 24)+theme(axis.title=element_blank(),
+                                 panel.grid.major = element_blank(), 
+                                 panel.grid.minor = element_blank(),
+                                 axis.text.y = element_blank(),
+                                 axis.ticks.y = element_blank())
+  
+g_R<-gathered_postR%>%ggplot(aes(y=key,x=value,col=as.factor(color)))+
+  geom_vline(xintercept = 0, linetype="dashed", color="dimgray")+
+  guides(fill=FALSE, color=FALSE)+
+  stat_pointinterval(.width=c(0.75,0.95))+
+  annotate("text", x=0.4, y=3.4, label= "slope_R", size=5)+  
+  theme_bw(base_size = 24)+theme(axis.title=element_blank(),
+                                 panel.grid.major = element_blank(), 
+                                 panel.grid.minor = element_blank(),
+                                 axis.text.y = element_blank(),
+                                 axis.ticks.y = element_blank())
+
+g_VR<-gathered_postVR%>%ggplot(aes(y=key,x=value,col=as.factor(color)))+
+  geom_vline(xintercept = 0, linetype="dashed", color="dimgray")+
+  guides(fill=FALSE, color=FALSE)+
+  stat_pointinterval(.width=c(0.75,0.95))+
+  annotate("text", x=-0.2, y=3.4, label= "slope_VR", size=5)+  
+  theme_bw(base_size = 24)+theme(axis.title=element_blank(),
+                                 panel.grid.major = element_blank(), 
+                                 panel.grid.minor = element_blank(),
+                                 axis.text.y = element_blank(),
+                                 axis.ticks.y = element_blank())
+
+g_A<-gathered_postA%>%ggplot(aes(y=key,x=value,col=as.factor(color)))+
+  geom_vline(xintercept = 0, linetype="dashed", color="dimgray")+
+  guides(fill=FALSE, color=FALSE)+
+  stat_pointinterval(.width=c(0.75,0.95))+
+  annotate("text", x=-0.1, y=3.4, label= "slope_A", size=5)+  
+  theme_bw(base_size = 24)+theme(axis.title=element_blank(),
+                                 panel.grid.major = element_blank(), 
+                                 panel.grid.minor = element_blank(),
+                                 axis.text.y = element_blank(),
+                                 axis.ticks.y = element_blank())
+
+g_uniA<-gathered_postuniA%>%ggplot(aes(y=key,x=value,col=as.factor(color)))+
+  geom_vline(xintercept = 0, linetype="dashed", color="dimgray")+
+  guides(fill=FALSE, color=FALSE)+
+  stat_pointinterval(.width=c(0.75,0.95))+
+  annotate("text", x=0.12, y=3.4, label= "slope_uniA", size=5)+  
+  theme_bw(base_size = 24)+theme(axis.title=element_blank(),
+                                 panel.grid.major = element_blank(), 
+                                 panel.grid.minor = element_blank(),
+                                 axis.text.y = element_blank(),
+                                 axis.ticks.y = element_blank())
+
+
+g_SR<-gathered_postSR%>%ggplot(aes(y=key,x=value,col=as.factor(color)))+
+  geom_vline(xintercept = 0, linetype="dashed", color="dimgray")+
+  guides(fill=FALSE, color=FALSE)+
+  stat_pointinterval(.width=c(0.75,0.95))+
+  annotate("text", x=0.5, y=3.4, label= "slope_SR", size=5)+  
+  theme_bw(base_size = 24)+theme(axis.title=element_blank(),
+                                 panel.grid.major = element_blank(), 
+                                 panel.grid.minor = element_blank(),
+                                 axis.text.y = element_blank(),
+                                 axis.ticks.y = element_blank())
+
+
+pdf("../../Results/gather_res/test/plot_posterior_pointinterval.pdf",height=6,width=12)
+op<-par(mar=c(2,2,2,2))
+gridExtra::grid.arrange(g_I,g_R,g_VR,g_A,g_uniA,g_SR,nrow=2)
+par(op)
+dev.off()
+
+#=================================================================================
+# Effect size of taxa plot
+#=================================================================================
+
+objc<-coef(full_model)
+objc$TAXA[,,"Intercept"]
+
+tx<-post%>%select(b_Intercept,
+                  `r_TAXA[fish,Intercept]`,
+                  `r_TAXA[freshwater.invertebrates,Intercept]`,
+                  `r_TAXA[freshwater.plants,Intercept]`,
+                  `r_TAXA[birds,Intercept]`,
+                  `r_TAXA[terrestrial.invertebrates,Intercept]`,
+                  `r_TAXA[terrestrial.plants,Intercept]`,
+                  `r_TAXA[mammals,Intercept]`
+                  )
+
+# following columns are the original source for objc$TAXA[,,"Intercept"]
+tx$fish<-tx$b_Intercept+tx$`r_TAXA[fish,Intercept]`
+tx$freshwater.inv<-tx$b_Intercept+tx$`r_TAXA[freshwater.invertebrates,Intercept]`
+tx$freshwater.plants<-tx$b_Intercept+tx$`r_TAXA[freshwater.plants,Intercept]`
+tx$birds<-tx$b_Intercept+tx$`r_TAXA[birds,Intercept]`
+tx$terrestrial.inv<-tx$b_Intercept+tx$`r_TAXA[terrestrial.invertebrates,Intercept]`
+tx$terrestrial.plants<-tx$b_Intercept+tx$`r_TAXA[terrestrial.plants,Intercept]`
+tx$mammals<-tx$b_Intercept+tx$`r_TAXA[mammals,Intercept]`
+
+tx_gather <-
+  tx %>% 
+  select(fish,freshwater.inv,freshwater.plants,
+         birds,terrestrial.inv,terrestrial.plants,mammals)%>%
+  gather() %>% 
+  mutate(key = factor(key, levels = c("fish", 
+                                      "freshwater.inv",
+                                      "freshwater.plants",
+                                      "birds",
+                                      "terrestrial.inv",
+                                      "terrestrial.plants",
+                                      "mammals")))
+
+g_tx<-tx_gather%>%ggplot(aes(y=key,x=value))+
+  geom_vline(xintercept = 0, linetype="dashed", color="dimgray")+
+  guides(fill=FALSE, color=FALSE)+
+  stat_pointinterval(.width=c(0.75,0.95))+
+  annotate("text", x=-1.6, y=1, label= "Effect of taxa on Intercept", size=4.5,col="blue")+  
+  theme_bw(base_size = 24)+theme(axis.title=element_blank(),
+                                 panel.grid.major = element_blank(), 
+                                 panel.grid.minor = element_blank(),
+                                 #axis.text.y = element_blank(),
+                                 axis.ticks.y = element_blank())
+
+pdf("../../Results/gather_res/test/plot_taxaeffect.pdf",height=5,width=8)
+op<-par(mar=c(2,2,2,2))
+g_tx
+par(op)
+dev.off()
+
+#=================================================================================
+# conditional effect plot
+#=================================================================================
+x<-conditional_effects(full_model)
+
+#plot conditional effects
+pdf("../../Results/gather_res/test/conditional_effects.pdf",height=12,width=16)
+op<-par(mfrow=c(3,2),mar=c(3,3,3,3))
+
+p1<-plot(x,points=F,plot=F)[[1]]+theme_classic()
+
+p2<-plot(x,points=F,plot=F)[[2]]+theme_classic()
+
+p3<-plot(x,points=F,plot=F)[[3]]+theme_classic()
+
+p4<-plot(x,points=F,plot=F)[[4]]+theme_classic()
+
+p5<-plot(x,points=F,plot=F)[[5]]+theme_classic()
+
+p6<-plot(x,points=F,plot=F)[[6]]+theme_classic()+
+  scale_color_manual(values=c("skyblue","green"))+
+  scale_fill_manual(values=c("skyblue","green"))
+
+p7<-plot(x,points=F,plot=F)[[7]]+theme_classic()+
+  scale_color_manual(values=c("skyblue","green"))+
+  scale_fill_manual(values=c("skyblue","green"))+
+  theme(legend.position = "none")
+
+p8<-plot(x,points=F,plot=F)[[8]]+theme_classic()+
+  scale_color_manual(values=c("skyblue","green"))+
+  scale_fill_manual(values=c("skyblue","green"))+
+  theme(legend.position = "none")
+
+p9<-plot(x,points=F,plot=F)[[9]]+theme_classic()+
+  scale_color_manual(values=c("skyblue","green"))+
+  scale_fill_manual(values=c("skyblue","green"))+
+  theme(legend.position = "none")
+
+p10<-plot(x,points=F,plot=F)[[10]]+theme_classic()+
+  scale_color_manual(values=c("skyblue","green"))+
+  scale_fill_manual(values=c("skyblue","green"))+
+  theme(legend.position = "none")
+
+p11<-plot(x,points=F,plot=F)[[11]]+theme_classic()+
+  scale_color_manual(values=c("skyblue","green"))+
+  scale_fill_manual(values=c("skyblue","green"))
+
+gridExtra::grid.arrange(p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11, nrow =3, ncol=4)
+par(op)
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
