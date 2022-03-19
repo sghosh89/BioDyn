c9fe879b653787cfbe77c2613f10a57daf95a653
sghosh89 <ghoshshyamolina89@gmail.com>
2022-03-19 19:54:35 +0100

    edits

--- R/gather_res/Master_gather_res.R
+++ R/gather_res/Master_gather_res.R
@@ -7,8 +7,8 @@ if(!dir.exists("../../Results/gather_res/")){
 
 #===============================================================
 source("summary_rank_category_for_each_realm.R") # to get interaction freq plot by realm for dominant sp pair
-source("summary_stability.R") # to get stability metric table
-source("summary_ancova_res.R") # run ancova for stability-synchrony relationship
+source("summary_stability.R") # to get stability metric table with all data
+#source("summary_ancova_res.R") # run ancova for stability-synchrony relationship
 #===============================================================
 
 source("datasummary.R") # summarizing metadata used in this study
