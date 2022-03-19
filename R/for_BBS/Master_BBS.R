1f6edc3d98f0d89787f0086413a9a8c3ddd1fafd
sghosh89 <ghoshshyamolina89@gmail.com>
2022-03-19 20:39:44 +0100

    edits

--- R/for_BBS/Master_BBS.R
+++ R/for_BBS/Master_BBS.R
@@ -2,6 +2,6 @@ path<-dirname(rstudioapi::getSourceEditorContext()$path)
 setwd(path)
 source("data_wrangling.R") # wrangling data and find good routes
 source("BBS_ta.R")         # tail analysis for each route
-source("BBS_ta_Diet5Cat.R") # comparing tail analysis results within and between guilds based on 5 diet categories
+#source("BBS_ta_Diet5Cat.R") 
 source("rank_category.R") # compute and save categorized interaction freq between dominant groups for each route
 source("summary_for_stabilitymetric.R") # get stability metric for BBS data
