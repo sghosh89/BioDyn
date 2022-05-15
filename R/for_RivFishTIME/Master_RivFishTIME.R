38e0699102c72f060f6a96eac45f1010226c6a20
Shyamolina <ghoshshyamolina89@gmail.com>
2022-05-15 20:30:33 +0200

    edits

--- R/for_RivFishTIME/Master_RivFishTIME.R
+++ R/for_RivFishTIME/Master_RivFishTIME.R
@@ -2,6 +2,6 @@ path<-dirname(rstudioapi::getSourceEditorContext()$path)
 setwd(path)
 source("wrangling_data.R") # clean and prepare data
 source("RivFishTIME.R") # tail analysis on each site
-source("get_summary_plot_regional.R") # summary plot of tail analysis result based on different region: US/ Europe
-source("rank_category.R") # compute and save categorized interaction freq between dominant groups for each site
+#source("get_summary_plot_regional.R") # summary plot of tail analysis result based on different region: US/ Europe
+#source("rank_category.R") # compute and save categorized interaction freq between dominant groups for each site
 source("summary_for_stabilitymetric.R") # for stability metric info
